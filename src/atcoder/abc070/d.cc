a4d44b7dfc63a2f2cce47823418aca7d1f086faf
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-08-13 14:59:02 +0900

    Participate ABC070

---
+++ src/atcoder/abc070/d.cc
@@ -0,0 +1,256 @@
+#include <iostream>
+#include <cstdint>
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <limits>
+#include <numeric>
+
+using namespace std;
+
+// Type aliases
+using i8  =  int8_t; using  u8 =  uint8_t;
+using i16 = int16_t; using u16 = uint16_t;
+using i32 = int32_t; using u32 = uint32_t;
+using i64 = int64_t; using u64 = uint64_t;
+
+template <class T> using V = vector<T>;
+
+// Loops
+#define REP(i, n) for (i64 i = 0; i < (n); ++i)
+#define REPR(i, n) for (i64 i = (n) - 1; i >= 0; --i)
+#define FOR(i, n, m) for (i64 i = (n); i < (m); ++i)
+#define FORR(i, n, m) for (i64 i = (m) - 1; i >= (n); --i)
+
+#define FORE(x, xs) for (auto &x: (xs))
+
+// Macros
+#define CTR(x) (x).begin(), (x).end()
+
+// Utils for Tuple
+namespace tuple_utils {
+    template<size_t...> struct seq{};
+
+    template<size_t N, size_t... Is>
+    struct gen_seq : gen_seq<N - 1, N - 1, Is...>{};
+
+    template<size_t... Is>
+    struct gen_seq<0, Is...> : seq<Is...>{};
+
+    template <class Tuple, size_t... Is>
+    void read(istream &stream, Tuple &t, seq<Is...>) {
+        static_cast<void>((int[]){0, (void(stream  >> get<Is>(t)), 0)...});
+    }
+
+    template<class Tuple, size_t... Is>
+    void print(ostream& stream, Tuple const& t, seq<Is...>) {
+        static_cast<void>((int[]){0, (void(stream << (Is == 0 ?  "" : ", ") << get<Is>(t)), 0)...});
+    }
+
+    template <size_t I, class F, class A, class... Elems>
+    struct ForEach {
+        void operator()(A &arg, tuple<Elems...> const& t) const {
+            F()(arg, get<I>(t));
+            ForEach<I - 1, F, A, Elems...>()(arg, t);
+        };
+        void operator()(A &arg, tuple<Elems...>& t) const {
+            F()(arg, get<I>(t));
+            ForEach<I - 1, F, A, Elems...>()(arg, t);
+        };
+    };
+    template <class F, class A, class... Elems>
+    struct ForEach<0, F, A, Elems...> {
+        void operator()(A &arg, tuple<Elems...> const& t) const {
+            F()(arg, get<0>(t));
+        };
+        void operator()(A &arg, tuple<Elems...>& t) const {
+            F()(arg, get<0>(t));
+        };
+    };
+    template <class F, class A, class... Elems>
+    void for_each(A &arg, tuple<Elems...> const& t) {
+        ForEach<tuple_size<tuple<Elems...>>::value - 1, F, A, Elems...>()(arg, t);
+    };
+    template <class F, class A, class... Elems>
+    void for_each(A &arg, tuple<Elems...>& t) {
+        ForEach<tuple_size<tuple<Elems...>>::value - 1, F, A, Elems...>()(arg, t);
+    };
+
+    struct hash_for_element {
+        template <class V>
+        void operator()(size_t &size, const V& v) const {
+            size ^= hash<V>()(v);
+        }
+    };
+}
+
+// Input
+template <class F, class S>
+istream &operator>>(istream &stream, pair<F, S> &pair) {
+    stream >> pair.first;
+    stream >> pair.second;
+    return stream;
+}
+template <class ...Args>
+istream &operator>>(istream &stream, tuple<Args...> &tuple) {
+    tuple_utils::read(stream, tuple, tuple_utils::gen_seq<sizeof...(Args)>());
+    return stream;
+}
+
+template <class T>
+T read() {
+    T t;
+    cin >> t;
+    return t;
+}
+template <class F, class S>
+pair<F, S> read() {
+    pair<F, S> p;
+    cin >> p;
+    return p;
+}
+template <class T1, class T2, class T3, class ...Args>
+tuple<T1, T2, T3, Args...> read() {
+    tuple<T1, T2, T3, Args...> t;
+    cin >> t;
+    return t;
+}
+
+template <class T>
+V<T> read(const int length) {
+    V<T> ts(length);
+    for (auto& t: ts) {
+        cin >> t;
+    }
+    return ts;
+}
+template <class F, class S>
+V<pair<F, S>> read(const int length) {
+    V<pair<F, S>> ps(length);
+    for (auto& p: ps) {
+        cin >> p;
+    }
+    return ps;
+}
+template <class T1, class T2, class T3, class ...Args>
+V<tuple<T1, T2, T3, Args...>> read(const int length) {
+    V<tuple<T1, T2, T3, Args...>> ts(length);
+    for (auto& t: ts) {
+        cin >> t;
+    }
+    return ts;
+}
+
+// Output
+namespace debug {
+    template <class F, class S>
+    ostream &operator<<(ostream& stream, const pair<F, S> &pair) {
+        stream << "{" << pair.first << ", " << pair.second << "}";
+        return stream;
+    }
+    template <class ...Args>
+    ostream &operator<<(ostream& stream, const tuple<Args...> &tuple) {
+        stream << "{";
+        tuple_utils::print(stream, tuple, tuple_utils::gen_seq<sizeof...(Args)>());
+        stream << "}";
+        return stream;
+    }
+
+    template <class T, class Alloc>
+    ostream &operator<<(ostream& stream, const vector<T, Alloc> &vector) {
+        stream << "[";
+
+        for (size_t i = 0; i < vector.size(); i++) {
+            stream << vector[i];
+
+            if (i != vector.size() - 1) {
+                stream << "," << ((i % 10 == 9) ? "\n " : "\t");
+            }
+        }
+
+        stream << "]";
+        return stream;
+    }
+}
+
+// Hash
+namespace std {
+    template <class F, class S>
+    struct hash<pair<F, S>> {
+        size_t operator ()(const pair<F, S> &p) const {
+            return hash<F>()(p.first) ^ hash<S>()(p.second);
+        }
+    };
+
+    template <class ...Args>
+    struct hash<tuple<Args...>> {
+        size_t operator ()(const tuple<Args...> &t) const {
+            size_t retval = 0;
+
+            tuple_utils::for_each<tuple_utils::hash_for_element, size_t, Args...>(retval, t);
+
+            return retval;
+        }
+    };
+}
+
+#define MAIN
+int body(int &argc, char **argv);
+
+// main function (DO NOT EDIT)
+int main (int argc, char **argv) {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    return body(argc, argv);
+}
+
+int body(int &argc, char **argv) {
+    auto N = read<i32>();
+    auto _edges = read<i32, i32, i64>(N - 1);
+    auto Q = read<i32>();
+    auto K = read<i32>();
+    auto qs = read<i32, i32>(Q);
+
+    auto edges = V<V<pair<i32, i64>>>(N);
+    FORE(edge, _edges) {
+        edges[get<0>(edge) - 1].push_back(make_pair(get<1>(edge) - 1, get<2>(edge)));
+        edges[get<1>(edge) - 1].push_back(make_pair(get<0>(edge) - 1, get<2>(edge)));
+    }
+
+    auto distance_from_K = V<i64>(N, -1);
+    distance_from_K[K - 1] = 0;
+    auto s = stack<i32>();
+    s.push(K - 1);
+
+    while (!s.empty()) {
+        auto x = s.top();
+        s.pop();
+
+        FORE (edge, edges[x]) {
+            auto d = edge.first;
+            auto cost = edge.second;
+
+            if (distance_from_K[d] != -1) {
+                continue ;
+            }
+
+            distance_from_K[d] = distance_from_K[x] + cost;
+
+            s.push(d);
+        }
+    }
+
+    REP (i, Q) {
+        auto q = qs[i];
+        auto a = q.first;
+        auto b = q.second;
+        cout << distance_from_K[a - 1] + distance_from_K[b - 1] << endl;
+    }
+
+    return 0;
+}
