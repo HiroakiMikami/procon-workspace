17948c89b5a38bffebe03d10b438d3ae54d94f0b
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-04-07 13:44:43 +0000

    AC ARC058 E

---
+++ src/atcoder/arc058/e.cc
@@ -0,0 +1,681 @@
+/*
+URL https://beta.atcoder.jp/contests/arc058/tasks/arc058_c
+SCORE 700
+AC true
+WA false
+TLE false
+MLE false
+TASK_TYPE 数え上げ mod DP bitDP
+FAILURE_TYPE 考察力不足 知識不足
+NOTES
+解説を読んだ。X-Y-Zを含まないものを引くのではという考察まではできたが、アルゴリズムに落とせなかった。
+また、アルゴリズムにするにはbitDPが必要だったので知識不足もある。
+
+覚えておく状態数が多いDPが出来上がりそう = bitDPと考えて良い？
+*/
+#include <iostream>
+#include <cstdint>
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <limits>
+#include <numeric>
+#include <iomanip>
+
+using namespace std;
+
+// Type aliases
+using i8  =  int8_t; using  u8 =  uint8_t;
+using i16 = int16_t; using u16 = uint16_t;
+using i32 = int32_t; using u32 = uint32_t;
+using i64 = int64_t; using u64 = uint64_t;
+
+template <class T> using V = vector<T>;
+
+// Loops
+#define REP(i, n) for (i64 i = 0; i < (n); ++i)
+#define REPR(i, n) for (i64 i = (n) - 1; i >= 0; --i)
+#define FOR(i, n, m) for (i64 i = (n); i < (m); ++i)
+#define FORR(i, n, m) for (i64 i = (m) - 1; i >= (n); --i)
+
+#define FORE(x, xs) for (auto &x: (xs))
+
+// Macros
+#define CTR(x) (x).begin(), (x).end()
+
+// Utils for Tuple
+namespace tuple_utils {
+    template<size_t...> struct seq{};
+
+    template<size_t N, size_t... Is>
+    struct gen_seq : gen_seq<N - 1, N - 1, Is...>{};
+
+    template<size_t... Is>
+    struct gen_seq<0, Is...> : seq<Is...>{};
+
+    template <class Tuple, size_t... Is>
+    void read(istream &stream, Tuple &t, seq<Is...>) {
+        static_cast<void>((int[]){0, (void(stream  >> get<Is>(t)), 0)...});
+    }
+
+    template<class Tuple, size_t... Is>
+    void print(ostream& stream, Tuple const& t, seq<Is...>) {
+        static_cast<void>((int[]){0, (void(stream << (Is == 0 ?  "" : ", ") << get<Is>(t)), 0)...});
+    }
+
+    template <size_t I, class F, class A, class... Elems>
+    struct ForEach {
+        void operator()(A &arg, tuple<Elems...> const& t) const {
+            F()(arg, get<I>(t));
+            ForEach<I - 1, F, A, Elems...>()(arg, t);
+        };
+        void operator()(A &arg, tuple<Elems...>& t) const {
+            F()(arg, get<I>(t));
+            ForEach<I - 1, F, A, Elems...>()(arg, t);
+        };
+    };
+    template <class F, class A, class... Elems>
+    struct ForEach<0, F, A, Elems...> {
+        void operator()(A &arg, tuple<Elems...> const& t) const {
+            F()(arg, get<0>(t));
+        };
+        void operator()(A &arg, tuple<Elems...>& t) const {
+            F()(arg, get<0>(t));
+        };
+    };
+    template <class F, class A, class... Elems>
+    void for_each(A &arg, tuple<Elems...> const& t) {
+        ForEach<tuple_size<tuple<Elems...>>::value - 1, F, A, Elems...>()(arg, t);
+    };
+    template <class F, class A, class... Elems>
+    void for_each(A &arg, tuple<Elems...>& t) {
+        ForEach<tuple_size<tuple<Elems...>>::value - 1, F, A, Elems...>()(arg, t);
+    };
+
+    struct hash_for_element {
+        template <class V>
+        void operator()(size_t &size, const V& v) const {
+            size ^= hash<V>()(v);
+        }
+    };
+}
+
+// STL support
+template <class Iterator>
+struct Container {
+    Container(const Iterator &begin, const Iterator &end) : m_begin(begin), m_end(end) {}
+    const Iterator& begin() const {
+        return this->m_begin;
+    }
+    const Iterator& end() const {
+        return this->m_end;
+    }
+    Iterator m_begin;
+    Iterator m_end;
+};
+
+template <class Functions>
+struct BaseIterator {
+    using State = typename Functions::State;
+
+    BaseIterator(const State &state, const Functions &func) : state(state), func(func) {
+        while (!this->is_end() && !this->is_valid()) {
+            this->next();
+        }
+    }
+    BaseIterator(const State &state) : state(state), func() {
+        while (!this->is_end() && !this->is_valid()) {
+            this->next();
+        }
+    }
+
+    decltype(auto) operator*() {
+        return this->func.get_value(this->state);
+    }
+
+    decltype(auto) operator*() const {
+        return this->func.get_value(this->state);
+    }
+
+    BaseIterator &operator++() {
+        if (this->is_end()) {
+            return *this;
+        }
+
+        this->next();
+        while (!this->is_end() && !this->is_valid()) {
+            this->next();
+        }
+
+        return *this;
+    }
+
+    BaseIterator &operator--() {
+        if (this->is_begin()) {
+            return *this;
+        }
+
+        this->previous();
+        while (!this->is_begin() && !this->is_valid()) {
+            this->previous();
+        }
+
+        return *this;
+    }
+
+    bool operator==(const BaseIterator<Functions> &rhs) const {
+        return this->state == rhs.state;
+    }
+    bool operator!=(const BaseIterator<Functions> &rhs) const {
+        return !(*this == rhs);
+    }
+
+    bool is_begin() const {
+        return this->func.is_begin(this->state);
+    }
+    bool is_end() const {
+        return this->func.is_end(this->state);
+    }
+    const State &get_state() const {
+        return this->state;
+    }
+private:
+    bool is_valid() const {
+        return this->func.is_valid(this->state);
+    }
+    void next() {
+        this->func.next(this->state);
+    }
+    void previous() {
+        this->func.previous(this->state);
+    }
+    State state;
+    Functions func;
+};
+
+// Input
+template <class F, class S>
+istream &operator>>(istream &stream, pair<F, S> &pair) {
+    stream >> pair.first;
+    stream >> pair.second;
+    return stream;
+}
+template <class ...Args>
+istream &operator>>(istream &stream, tuple<Args...> &tuple) {
+    tuple_utils::read(stream, tuple, tuple_utils::gen_seq<sizeof...(Args)>());
+    return stream;
+}
+
+template <class T>
+T read() {
+    T t;
+    cin >> t;
+    return t;
+}
+template <class F, class S>
+pair<F, S> read() {
+    pair<F, S> p;
+    cin >> p;
+    return p;
+}
+template <class T1, class T2, class T3, class ...Args>
+tuple<T1, T2, T3, Args...> read() {
+    tuple<T1, T2, T3, Args...> t;
+    cin >> t;
+    return t;
+}
+
+template <class T>
+V<T> read(const int length) {
+    V<T> ts(length);
+    for (auto& t: ts) {
+        cin >> t;
+    }
+    return ts;
+}
+template <class F, class S>
+V<pair<F, S>> read(const int length) {
+    V<pair<F, S>> ps(length);
+    for (auto& p: ps) {
+        cin >> p;
+    }
+    return ps;
+}
+template <class T1, class T2, class T3, class ...Args>
+V<tuple<T1, T2, T3, Args...>> read(const int length) {
+    V<tuple<T1, T2, T3, Args...>> ts(length);
+    for (auto& t: ts) {
+        cin >> t;
+    }
+    return ts;
+}
+
+// Output
+namespace debug {
+    template <class F, class S>
+    ostream &operator<<(ostream& stream, const pair<F, S> &pair) {
+        stream << "{" << pair.first << ", " << pair.second << "}";
+        return stream;
+    }
+    template <class ...Args>
+    ostream &operator<<(ostream& stream, const tuple<Args...> &tuple) {
+        stream << "{";
+        tuple_utils::print(stream, tuple, tuple_utils::gen_seq<sizeof...(Args)>());
+        stream << "}";
+        return stream;
+    }
+
+    template <class Iterator>
+    Container<Iterator> container(const Iterator &begin, const Iterator &end) {
+        return Container<Iterator>(begin, end);
+    }
+
+    template <class Iterator>
+    ostream &operator<<(ostream &stream, const Container<Iterator> &container) {
+        stream << "[";
+
+        size_t cnt = 0;
+        for (const auto &it: container) {
+            stream << it;
+            stream << "," << ((cnt % 10 == 9) ? "\n " : "\t");
+
+            cnt += 1;
+        }
+
+        stream << "\b\b]";
+        return stream;
+    }
+
+    template <class T, class Alloc>
+    ostream &operator<<(ostream& stream, const vector<T, Alloc> &vector) {
+        return stream << container(vector.begin(), vector.end());
+    }
+}
+
+// Hash
+namespace std {
+    template <class F, class S>
+    struct hash<pair<F, S>> {
+        size_t operator ()(const pair<F, S> &p) const {
+            return hash<F>()(p.first) ^ hash<S>()(p.second);
+        }
+    };
+
+    template <class ...Args>
+    struct hash<tuple<Args...>> {
+        size_t operator ()(const tuple<Args...> &t) const {
+            size_t retval = 0;
+
+            tuple_utils::for_each<tuple_utils::hash_for_element, size_t, Args...>(retval, t);
+
+            return retval;
+        }
+    };
+}
+#define MAIN
+void body();
+
+// main function (DO NOT EDIT)
+int main (int argc, char **argv) {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cout << fixed;
+    body();
+
+    return 0;
+}
+
+#ifndef MAIN
+#include "common.cc"
+#endif
+
+/*
+ * n^r
+ */
+template<class V>
+static V pow(V n, i64 r) {
+    if (r == 0) {
+        return 1;
+    }
+
+    auto r2 = r / 2;
+    auto x2 = pow(n, r2);
+
+    return x2 * x2 * ((r % 2 == 0) ? 1 : n);
+}
+
+static i64 gcd(i64 a, i64 b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+template<class Iterator>
+static i64 gcd_ctr(const Iterator &begin, const Iterator &end) {
+    if (begin == end) {
+        return -1;
+    } else {
+        auto ans = *begin;
+        auto it = begin;
+        ++it;
+        for (; it != end; ++it) {
+            auto x = *it;
+            ans = gcd(ans, x);
+        }
+        return ans;
+    }
+}
+
+static i64 gcd_ctr(const V<i64> &xs) {
+    return gcd_ctr(xs.begin(), xs.end());
+}
+
+static i64 lcm(i64 a, i64 b) {
+    auto x = gcd(a, b);
+    return a / x * b;
+}
+
+template<class Iterator>
+static i64 lcm_ctr(const Iterator &begin, const Iterator &end) {
+    if (begin == end) {
+        return -1;
+    } else {
+        auto ans = *begin;
+        auto it = begin;
+        ++it;
+        for (; it != end; ++it) {
+            auto x = *it;
+            ans = lcm(ans, x);
+        }
+        return ans;
+    }
+}
+
+static i64 lcm_ctr(const V<i64> &xs) {
+    return lcm_ctr(xs.begin(), xs.end());
+}
+
+template<class V>
+static V combination(V n, i64 r) {
+    if (r == 0) {
+        return 1;
+    }
+
+    V x = 1;
+    FOR (d, 1, r + 1) {
+        x *= n;
+        n -= 1;
+        x /= d;
+    }
+    return x;
+}
+
+static bool is_prime(i64 n) {
+    if (n <= 1) return false;
+    for (i64 i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+static V<i64> divisor(i64 n) {
+    V<i64> res;
+    for (i64 i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i != n / i) res.push_back(n / i);
+        }
+    }
+    return res;
+}
+
+static unordered_map<i64, size_t> prime_factor(i64 n) {
+    unordered_map<i64, size_t> res;
+    for (i64 i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            res[i] += 1;
+            n /= i;
+        }
+    }
+    if (n != 1) res[n] = 1;
+    return res;
+}
+
+static pair<V<i64>, V<bool>> sieve(i64 n) {
+    V<i64> prime;
+    V<bool> is_prime_(n + 1, true);
+    is_prime_[0] = is_prime_[1] = false;
+
+    FOR (i, 2, n + 1) {
+        if (is_prime_[i]) {
+            prime.push_back(i);
+            for (i64 j = 2 * i; j <= n; j += i) {
+                is_prime_[j] = false;
+            }
+        }
+    }
+    return {prime, is_prime_};
+}
+
+#ifndef MAIN
+#include "common.cc"
+#include "math.cc"
+#endif
+
+namespace mod {
+    const i64 MOD = 1000000007;
+
+    struct Integer {
+        Integer() : value(0) {}
+        Integer(i64 value) {
+            update_value((value));
+        }
+
+        Integer inverse() const {
+            return pow(*this, 1000000005);
+        }
+
+        Integer operator+(const Integer &rhs) const {
+            return this->value + rhs.value;
+        }
+        Integer operator-(const Integer &rhs) const {
+            return this->value - rhs.value;
+        }
+
+        Integer operator*(const Integer &rhs) const {
+            return this->value * rhs.value;
+        }
+
+        Integer operator /(const Integer &rhs) const {
+            return this->value * rhs.inverse().value;
+        }
+
+        void operator +=(const Integer &rhs) {
+            update_value(this->value + rhs.value);
+        }
+
+        void operator -=(const Integer &rhs) {
+            update_value(this->value - rhs.value);
+        }
+
+        void operator *=(const Integer &rhs) {
+            update_value(this->value * rhs.value);
+        }
+
+        void operator /=(const Integer &rhs) {
+            update_value(this->value * rhs.inverse().value);
+        }
+
+        bool operator==(const Integer &rhs) const {
+            return this->value == rhs.value;
+        }
+        template <class V>
+        bool operator==(V rhs) const {
+            return this->value == rhs;
+        }
+
+        bool operator!=(const Integer &rhs) const {
+            return this->value != rhs.value;
+        }
+        template <class V>
+        bool operator!=(V rhs) const {
+            return this->value != rhs;
+        }
+
+        const i64 &get() const {
+            return this->value;
+        }
+
+    private:
+        void update_value(i64 new_value) {
+            this->value = new_value;
+            if (this->value < 0) {
+                this->value += MOD;
+            }
+            if (this->value >= MOD) {
+                this->value %= MOD;
+            }
+        }
+        i64 value;
+    };
+
+    template <class V>
+    static bool operator==(V lhs, const Integer &rhs) {
+        return rhs ==lhs;
+    }
+    template <class V>
+    static bool operator!=(V lhs, const Integer &rhs) {
+        return rhs != lhs;
+    }
+
+    struct fact {
+        Integer value;
+        Integer inverse;
+    };
+
+    static V<fact> fact_table(const size_t n) {
+        V<fact> retval(n + 1);
+
+        retval[0].value = 1;
+        FOR(i, 1, n + 1) {
+            retval[i].value = retval[i - 1].value * i;
+        }
+
+        retval[n].inverse = retval[n].value.inverse();
+        REPR(i, n) {
+            retval[i].inverse = retval[i + 1].inverse * (i + 1);
+        }
+
+        return retval;
+    }
+}
+
+void body() {
+    using namespace debug;
+    auto N = read<i64>();
+    auto X = read<i64>();
+    auto Y = read<i64>();
+    auto Z = read<i64>();
+
+    using namespace mod;
+    /*
+     * 数字iを1とその後に続くi-1個の0で表現する
+     * 例えば数列 1 2 3 をエンコードすると 1 10 100 <0bit目>となる
+     */
+    V<V<i8>> f(1 << (X + Y + Z - 1), V<i8>()); // f[i] = iを数列として見たとき、その後ろに足すとX-Y-Zにならない数
+    for (u32 i = 0; i < f.size(); ++i) {
+        // 数列のデコード（逆順）
+        V<i64> as;
+        as.reserve(X + Y + Z - 1);
+        auto n = 0;
+        REP (j, X + Y + Z) {
+            n += 1;
+            if ((i >> j & 1) == 1) {
+                // j bit目が1
+                as.push_back(n);
+                n = 0;
+            }
+        }
+
+        FOR (j, 1, 11) {
+            // 末尾にjを追加してX-Y-Zになるか
+            i64 cnt = j;
+            int target = 0;
+            bool flag = false;
+
+            if (cnt == Z) {
+                cnt = 0;
+                target = 1;
+            }
+
+            FORE (a, as) {
+                cnt += a;
+                if (target == 0) {
+                    // Z
+                    if (cnt == Z) {
+                        cnt = 0;
+                        target = 1;
+                    }
+                } else if (target == 1) {
+                    // Y
+                    if (cnt == Y) {
+                        cnt = 0;
+                        target = 2;
+                    }
+                } else {
+                    // X
+                    if (cnt == X) {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!flag) {
+                f[i].push_back(j);
+            }
+        }
+    }
+
+
+    V<V<Integer>> dp(N + 1, V<Integer>(1 << (X + Y + Z  - 1), Integer(0))); // DP[i][j] = 長さiで末尾がjの数列で、X-Y-Zを含まないものの個数
+    dp[0][0] = 1; // 空数列が条件を満たす
+    REP (i, N) {
+        REP (j, dp[i].size()) {
+            FORE (n, f[j]) {
+                // 末尾にnを追加する
+                auto x = j << n;
+                x = x | (1 << (n - 1));
+                x = x % dp[i + 1].size(); // 下位(X + Y + Z - 1) bitだけとる
+                dp[i + 1][x] += dp[i][j];
+            }
+        }
+    }
+
+    Integer x(0);
+    FORE(n, dp[N]) {
+        x += n;
+    }
+
+#if 0
+    REP (i, N + 1) {
+        i64 n = 0;
+        REP(y, dp[i].size()) {
+            if (i == 1 && dp[i][y].get() != 0) {
+                cout << n << " (" << i << ", " << y << ") " << dp[i][y].get() << endl;
+            }
+            n += dp[i][y].get();
+        }
+        cout << i << " " << n << endl;
+    }
+#endif
+    Integer ans = pow(Integer(10), N);
+    ans -= x;
+    cout << ans.get() << endl;
+
+}
