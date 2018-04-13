ba32b8d3fbc48fac9f3850ce560e8d1a18cb15e5
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-04-13 12:34:35 +0000

    Add metadata to AGC021

--- src/atcoder/agc021/a.cc
+++ src/atcoder/agc021/a.cc
@@ -1,3 +1,14 @@
+/*
+URL https://beta.atcoder.jp/contests/agc021/tasks/agc021_a
+SCORE 300
+AC true
+WA false
+TLE false
+MLE false
+TASK_TYPE 基本 10進数
+FAILURE_TYPE
+NOTES
+*/
 #include <iostream>
 #include <cstdint>
 #include <utility>
