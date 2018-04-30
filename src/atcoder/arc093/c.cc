2d64e525442a989298b402d4c0fd942e4dc06cc3
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-04-30 07:21:04 +0000

    Add metadata to ARC093

--- src/atcoder/arc093/c.cc
+++ src/atcoder/arc093/c.cc
@@ -1,3 +1,14 @@
+/*
+URL https://beta.atcoder.jp/contests/arc093/tasks/arc093_a
+SCORE 300
+AC true
+WA false
+TLE false
+MLE false
+TASK_TYPE 累積和
+FAILURE_TYPE
+NOTES
+*/
 #include <iostream>
 #include <cstdint>
 #include <utility>
