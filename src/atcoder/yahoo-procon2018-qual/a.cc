4f0ffad968090c3e07e34df90c12e7d23e56f2df
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-05-04 00:47:00 +0000

    Add metadata to Yahoo Procol 2018 Qual

--- src/atcoder/yahoo-procon2018-qual/a.cc
+++ src/atcoder/yahoo-procon2018-qual/a.cc
@@ -1,3 +1,14 @@
+/*
+URL https://beta.atcoder.jp/contests/yahoo-procon2018-qual/tasks/yahoo_procon2018_qual_a
+SCORE 100
+AC true
+WA false
+TLE false
+MLE false
+TASK_TYPE 基本
+FAILURE_TYPE
+NOTES
+*/
 #include <iostream>
 #include <cstdint>
 #include <utility>
