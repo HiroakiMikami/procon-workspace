87a621fbaf1681a5f806a982ed1da0504f9e7208
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-06-02 04:30:07 +0000

    [procon test] Run test src/atcoder/arc068/e.cc

--- src/atcoder/arc068/e.cc
+++ src/atcoder/arc068/e.cc
@@ -581,133 +581,78 @@ void body();
 #include "common.cc"
 #endif
 
-/*
- * n^r
- */
-template<class V>
-static V pow(V n, i64 r) {
-    if (r == 0) {
-        return 1;
-    }
-
-    auto r2 = r / 2;
-    auto x2 = pow(n, r2);
-
-    return x2 * x2 * ((r % 2 == 0) ? 1 : n);
-}
-
-static i64 gcd(i64 a, i64 b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-
-template<class Iterator>
-static i64 gcd_ctr(const Iterator &begin, const Iterator &end) {
-    if (begin == end) {
-        return -1;
-    } else {
-        auto ans = *begin;
-        auto it = begin;
-        ++it;
-        for (; it != end; ++it) {
-            auto x = *it;
-            ans = gcd(ans, x);
-        }
-        return ans;
-    }
-}
-
-static i64 gcd_ctr(const V<i64> &xs) {
-    return gcd_ctr(xs.begin(), xs.end());
-}
-
-static i64 lcm(i64 a, i64 b) {
-    auto x = gcd(a, b);
-    return a / x * b;
-}
-
-template<class Iterator>
-static i64 lcm_ctr(const Iterator &begin, const Iterator &end) {
-    if (begin == end) {
-        return -1;
-    } else {
-        auto ans = *begin;
-        auto it = begin;
-        ++it;
-        for (; it != end; ++it) {
-            auto x = *it;
-            ans = lcm(ans, x);
-        }
-        return ans;
-    }
-}
-
-static i64 lcm_ctr(const V<i64> &xs) {
-    return lcm_ctr(xs.begin(), xs.end());
-}
-
-template<class V>
-static V combination(V n, i64 r) {
-    if (r == 0) {
-        return 1;
-    }
-
-    V x = 1;
-    FOR (d, 1, r + 1) {
-        x *= n;
-        n -= 1;
-        x /= d;
-    }
-    return x;
-}
-
-static bool is_prime(i64 n) {
-    if (n <= 1) return false;
-    for (i64 i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
-
-static V<i64> divisor(i64 n) {
-    V<i64> res;
-    for (i64 i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            res.push_back(i);
-            if (i != n / i) res.push_back(n / i);
-        }
-    }
-    return res;
-}
-
-static unordered_map<i64, size_t> prime_factor(i64 n) {
-    unordered_map<i64, size_t> res;
-    for (i64 i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
-            res[i] += 1;
-            n /= i;
-        }
-    }
-    if (n != 1) res[n] = 1;
-    return res;
-}
-
-static pair<V<i64>, V<bool>> sieve(i64 n) {
-    V<i64> prime;
-    V<bool> is_prime_(n + 1, true);
-    is_prime_[0] = is_prime_[1] = false;
-
-    FOR (i, 2, n + 1) {
-        if (is_prime_[i]) {
-            prime.push_back(i);
-            for (i64 j = 2 * i; j <= n; j += i) {
-                is_prime_[j] = false;
+template <class T>
+struct BinaryIndexedTree {
+    BinaryIndexedTree() : m_bit(0, 0) {}
+    BinaryIndexedTree(const size_t size) : m_bit(size, 0) {}
+    BinaryIndexedTree(const vector<T> &vector) : m_bit(vector) {
+        REP (i, this->m_bit.size()) {
+            auto j = i + lsb(i + 1);
+            if (j < this->m_bit.size()) {
+                this->m_bit[j] += this->m_bit[i];
             }
         }
     }
-    return {prime, is_prime_};
-}
+    template <class Iterator>
+    BinaryIndexedTree(const Iterator &begin, const Iterator &end) : m_bit(distance(begin, end), 0) {
+        auto it = begin;
+        REP (i, this->m_bit.size()) {
+            this->m_bit[i] = *it;
+            auto j = i + lsb(i + 1);
+            if (j < this->m_bit.size()) {
+                this->m_bit[j] += this->m_bit[i];
+            }
 
+            ++it;
+        }
+    }
+
+    void add(size_t index, const T& delta) {
+        while (index < this->size()) {
+            this->m_bit[index] += delta;
+            index += lsb(index + 1);
+        }
+    }
+    auto sum(size_t length) const {
+        size_t index = length;
+        auto retval = T();
+        while (index != 0) {
+            retval += this->m_bit[index - 1];
+            index -= lsb(index);
+        }
+        return retval;
+    }
+    auto sum(size_t begin, size_t end) const {
+        // sum between begin ... end-1
+        return this->sum(end) - this->sum(begin);
+    }
+    auto at(size_t index) const {
+        return this->sum(index, index + 1);
+    }
+
+    size_t size() const {
+        return this->m_bit.size();
+    }
+
+    auto begin() const {
+        return this->m_bit.cbegin();
+    }
+    auto cbegin() const {
+        return this->m_bit.cbegin();
+    }
+    auto end() const {
+        return this->m_bit.cend();
+    }
+    auto cend() const {
+        return this->m_bit.cend();
+    }
+private:
+    static auto lsb(size_t i) {
+        return i & -i;
+    }
+    vector<T> m_bit;
+};
+template <class T> using BIT = BinaryIndexedTree<T>;
 
 // main function (DO NOT EDIT)
 int main (int argc, char **argv) {
@@ -725,17 +670,18 @@ void body() {
     auto M = read<i64>();
     auto lr = read<i64, i64>(N);
 
+    BIT<i64> X(M + 2);
+    EACH (elem, lr) {
+        X.add(elem.first, 1);
+        X.add(elem.second + 1, -1);
+    }
+
     // 愚直解
     FOR (m, 1, M + 1) {
         i64 ans = 0;
         REP (i, N) {
             if (lr[i].first <= lr[i].second - (lr[i].second % m)) {
                 ans += 1;
-            }
-            if (lr[i].first % m == 0) {
-                //ans += 1;
-            } else if (lr[i].first / m != lr[i].second / m) {
-                //ans += 1;
             }
         }
         cout << ans << endl;
