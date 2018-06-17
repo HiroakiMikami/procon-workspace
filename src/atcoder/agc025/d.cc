29f573e34e21fd8afadeba77b0a57b33584add57
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-06-17 10:33:29 +0000

    [procon test] Run test src/atcoder/agc025/d.cc

--- src/atcoder/agc025/d.cc
+++ src/atcoder/agc025/d.cc
@@ -1132,6 +1132,7 @@ void body() {
     REP (s, 2 * N) {
         FOR (t, s, 2 * N) {
             REP (i, 2 * N) {
+                if (D1 - i * i < 0 && D2 - i *i < 0) break;
                 i64 j_1 = static_cast<i64>(std::sqrt(D1 - i * i));
                 i64 j_2 = static_cast<i64>(std::sqrt(D2 - i * i));
 
