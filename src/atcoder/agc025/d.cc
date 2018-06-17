77c16e55a5540e73ffdfd4a6420bb060d66e1c21
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-06-17 09:00:35 +0000

    [procon test] Run test src/atcoder/agc025/d.cc

--- src/atcoder/agc025/d.cc
+++ src/atcoder/agc025/d.cc
@@ -1110,11 +1110,15 @@ void body() {
 
                 if (i * i + j_1 * j_1 == D1 && y + j_1 < 2 * N) {
                     g_1.add_edge(make_tuple(id1, to_id(x + i, y + j_1)));
-                    g_1.add_edge(make_tuple(id2, to_id(y + j_1, x + i)));
+                    if (id1 != id2) {
+                        g_1.add_edge(make_tuple(id2, to_id(y + j_1, x + i)));
+                    }
                 }
                 if (i * i + j_2 * j_2 == D2 && y + j_2 < 2 * N) {
                     g_2.add_edge(make_tuple(id1, to_id(x + i, y + j_2)));
-                    g_2.add_edge(make_tuple(id2, to_id(y + j_2, x + i)));
+                    if (id1 != id2) {
+                        g_2.add_edge(make_tuple(id2, to_id(y + j_2, x + i)));
+                    }
                 }
             }
         }
