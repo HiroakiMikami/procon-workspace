3a6ca2173f84f2429376937c5c656ab5d3af9297
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-06-20 10:38:45 +0000

    [procon test] Run test src/atcoder/abc054/c.cc

--- src/atcoder/abc054/c.cc
+++ src/atcoder/abc054/c.cc
@@ -1143,7 +1143,7 @@ void body() {
     do {
         bool flag = true;
         REP (i, N - 1) {
-            auto v1 = (i == 0) ? 0 : s[i - 1];
+            auto v1 = (i == 0) ? 0 : vs[i - 1];
             auto v2 = vs[i];
 
             if (!g.has_edge(v1, v2)) {
