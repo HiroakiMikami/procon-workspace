24e795c1bd322922a91a0ab64c7a1733b8d48cb0
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-06-23 00:25:49 +0000

    AC ARC086 C

--- src/atcoder/arc086/c.cc
+++ src/atcoder/arc086/c.cc
@@ -1,11 +1,11 @@
 /*
-URL https://
-SCORE 0
-AC false
+URL https://beta.atcoder.jp/contests/arc086/tasks/arc086_a
+SCORE 300
+AC true
 WA false
 TLE false
 MLE false
-TASK_TYPE
+TASK_TYPE 最大化・最小化 ソート シミュレーション
 FAILURE_TYPE
 NOTES
 */
