476822e058900270460e0f7cc2c304bdb257916a
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-01 05:31:43 +0000

    AC AGC010 A

--- src/atcoder/agc010/a.cc
+++ src/atcoder/agc010/a.cc
@@ -1,11 +1,11 @@
 /*
-URL https://
-SCORE 0
-AC false
+URL https://beta.atcoder.jp/contests/agc010/tasks/agc010_a
+SCORE 300
+AC true
 WA false
 TLE false
 MLE false
-TASK_TYPE
+TASK_TYPE 判定 整数
 FAILURE_TYPE
 NOTES
 */
