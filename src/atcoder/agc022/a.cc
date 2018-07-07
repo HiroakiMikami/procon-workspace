e3c6e4cc6cee51cf01c5f43ac06565a75eb6b98d
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-07 07:12:21 +0000

    AC AGC022 A

--- src/atcoder/agc022/a.cc
+++ src/atcoder/agc022/a.cc
@@ -1,11 +1,11 @@
 /*
-URL https://
-SCORE 0
-AC false
+URL https://beta.atcoder.jp/contests/agc022/tasks/agc022_a
+SCORE 300
+AC true
 WA false
 TLE false
 MLE false
-TASK_TYPE
+TASK_TYPE 辞書順 全探索
 FAILURE_TYPE
 NOTES
 */
