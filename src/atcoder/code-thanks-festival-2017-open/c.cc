173fe1e3b92933bc0e4e4767a8fc33b09d32db42
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-08 00:21:35 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2017-open/c.cc

--- src/atcoder/code-thanks-festival-2017-open/c.cc
+++ src/atcoder/code-thanks-festival-2017-open/c.cc
@@ -821,6 +821,7 @@ void body() {
     i64 ans = 0;
     REP (i, K) {
         auto n = time.top();
+        dump(n);
         time.pop();
         ans += n.first;
         time.emplace(n.first + n.second, n.second);
