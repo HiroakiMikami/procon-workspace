c15ab25e192800c17aa862fa02787c8216fc2eb2
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-14 00:02:13 +0000

    [procon test] Run test src/atcoder/ddcc2017-final/a.cc

--- src/atcoder/ddcc2017-final/a.cc
+++ src/atcoder/ddcc2017-final/a.cc
@@ -824,7 +824,7 @@ void body() {
             }
             dump(Y, "center");
 
-            ans += 1 + (Y * 2 - K) / (2 * K);
+            ans += 1 + (Y * 2 - K) / (2 * K) * 2;
 
             //// 左右
             REP (i, (n - 1) / 2) {
@@ -839,7 +839,7 @@ void body() {
                     }
                 }
                 dump(Y, x);
-                ans += (1 + (Y * 2 - K) / (2 * K)) * 2;
+                ans += (1 + (Y * 2 - K) / (2 * K) * 2) * 2;
             }
 
         } else {
