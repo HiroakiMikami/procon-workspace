1829b7cebcda1bc3ebc36cb15d5d28e13d242f54
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-15 05:11:08 +0000

    [procon test] Run test src/atcoder/abc051/d.cc

--- src/atcoder/abc051/d.cc
+++ src/atcoder/abc051/d.cc
@@ -1251,7 +1251,7 @@ namespace graph {
                         auto prev_opt = distance[k][j].previous_vertex;
                         distance[i][j] = C(distance[i][k].cost + distance[k][j].cost, prev_opt);
                         if (prev_opt.empty()) {
-                            distance[i][j].push_back(k);
+                            distance[i][j].previous_vertex.push_back(k);
                         }
                     } else if (distance[i][k].cost != max && distance[k][j].cost != max &&
                         distance[i][j].cost == distance[i][k].cost + distance[k][j].cost) {
