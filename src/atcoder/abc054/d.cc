00cb7b715c2e8ab203ae0e73d22a831b39f3e94a
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-15 12:34:33 +0000

    [procon test] Run test src/atcoder/abc054/d.cc

--- src/atcoder/abc054/d.cc
+++ src/atcoder/abc054/d.cc
@@ -829,16 +829,17 @@ void body() {
         auto a = get<0>(abcs[i]);
         auto b = get<1>(abcs[i]);
         auto c = get<2>(abcs[i]);
-        FOR (A, a, N * 10 + 1) {
-            FOR (B, b, N * 10 + 1) {
-                if (dp[i][A][B] != -1) {
-                    dp[i + 1][A][B] = dp[i][A][B];
-                }
-                if (dp[i][A - a][B - b] != -1) {
-                    if (dp[i + 1][A][B] == -1) {
-                        dp[i + 1][A][B] = dp[i][A - a][B - b] + c;
-                    } else {
-                        dp[i + 1][A][B] = std::min(dp[i + 1][A][B], dp[i][A - a][B - b] + c);
+        REP (A, N * 10 + 1) {
+            REP (B, N * 10 + 1) {
+                // dp[i + 1][A][B]の更新
+                dp[i + 1][A][B] = dp[i][A][B];
+                if (A - a >= 0 && B - b >= 0) {
+                    if (dp[i][A - a][B - b] != -1) {
+                        if (dp[i + 1][A][B] == -1) {
+                            dp[i + 1][A][B] = dp[i][A - a][B - b] + c;
+                        } else {
+                            dp[i + 1][A][B] = std::min(dp[i + 1][A][B], dp[i][A - a][B - b] + c);
+                        }
                     }
                 }
             }
