9a37ebae295d5f3f0e1f9b1f37ac152d43236489
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-19 11:58:47 +0000

    [procon test] Run test src/atcoder/abc089/d.cc

--- src/atcoder/abc089/d.cc
+++ src/atcoder/abc089/d.cc
@@ -857,8 +857,8 @@ void body() {
         auto n2 = (R - x) / D;
 
         dump(x, n1, n2, ns[x][n2], ns[x][n1]);
-        dump(n2p[L], n2p[R]);
 
-        cout << ns[x][n2] - ns[x][n1] << endl;
+
+        cout << ns[x][n1] - ns[x][n2] << endl;
     }
 }
