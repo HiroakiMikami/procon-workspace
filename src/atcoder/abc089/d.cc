d2c8109130dc9c9dfed4023053be13597cd7533f
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-07-19 12:00:00 +0000

    [procon test] Run test src/atcoder/abc089/d.cc

--- src/atcoder/abc089/d.cc
+++ src/atcoder/abc089/d.cc
@@ -843,6 +843,7 @@ void body() {
             auto p2 = n2p[y];
             auto m = std::abs(p2.first - p.first) + std::abs(p2.second - p.second);
 
+            dump(x, y, m);
             ns[x][n] = ns[x][n - 1] + m;
             n += 1;
         }
