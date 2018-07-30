a0f0b7df77fb841ce46ab87ff6c5f406db925573
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2018-07-30 19:42:38 +0900

    AC AGC011 B

--- src/atcoder/agc011/b.cc
+++ src/atcoder/agc011/b.cc
@@ -1,11 +1,11 @@
 /*
-URL https://
-SCORE 0
-AC false
+URL https://beta.atcoder.jp/contests/agc011/tasks/agc011_b
+SCORE 400
+AC true
 WA false
 TLE false
 MLE false
-TASK_TYPE
+TASK_TYPE ソート DP 累積和
 FAILURE_TYPE
 NOTES
 */
