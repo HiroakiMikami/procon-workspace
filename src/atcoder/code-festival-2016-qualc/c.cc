a084f974eacb17d79a951efa0ac53793fb68d2b1
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2018-08-08 19:44:40 +0900

    [procon test] Run test src/atcoder/code-festival-2016-qualc/c.cc

--- src/atcoder/code-festival-2016-qualc/c.cc
+++ src/atcoder/code-festival-2016-qualc/c.cc
@@ -1081,7 +1081,7 @@ void body() {
             cands[i].second = Ts[i];
         } else {
             cands[i].first = (Ts[i] > cands[i - 1].second) ? Ts[i] : 1;
-            cands[i].second = (Ts[i] > cands[i - 1].second) ? Ts[i] : cands[i].first;
+            cands[i].second = (Ts[i] > cands[i - 1].second) ? Ts[i] : cands[i].second;
         }
     }
 
@@ -1092,7 +1092,7 @@ void body() {
         } else {
             i64 x = (As[i] > cands[i + 1].second) ? As[i] : 1;
             cands[i].first = std::max(cands[i].first, x);
-            i64 y = (As[i] > cands[i + 1].second) ? As[i] : cands[i].first;
+            i64 y = (As[i] > cands[i + 1].second) ? As[i] : cands[i].second;
             cands[i].second = std::min(y, As[i]);
         }
     }
