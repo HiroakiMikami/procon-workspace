2389ff84f7ba94187e4461a482085aca274a2ee3
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-09-20 06:17:56 +0000

    [procon test] Run test src/atcoder/abc106/d.cc

--- src/atcoder/abc106/d.cc
+++ src/atcoder/abc106/d.cc
@@ -809,6 +809,96 @@ int main (int argc, char **argv) {
     return 0;
 }
 
+#ifndef MAIN
+#include "common.cc"
+#endif
+
+template <class T>
+struct BinaryIndexedTree {
+    BinaryIndexedTree() : m_bit(0, 0) {}
+    BinaryIndexedTree(const size_t size) : m_bit(size, 0) {}
+    BinaryIndexedTree(const vector<T> &vector) : m_bit(vector) {
+        REP (i, this->m_bit.size()) {
+            auto j = i + lsb(i + 1);
+            if (j < this->m_bit.size()) {
+                this->m_bit[j] += this->m_bit[i];
+            }
+        }
+    }
+    template <class Iterator>
+    BinaryIndexedTree(const Iterator &begin, const Iterator &end) : m_bit(distance(begin, end), 0) {
+        auto it = begin;
+        REP (i, this->m_bit.size()) {
+            this->m_bit[i] = *it;
+            auto j = i + lsb(i + 1);
+            if (j < this->m_bit.size()) {
+                this->m_bit[j] += this->m_bit[i];
+            }
+
+            ++it;
+        }
+    }
+
+    void add(size_t index, const T& delta) {
+        while (index < this->size()) {
+            this->m_bit[index] += delta;
+            index += lsb(index + 1);
+        }
+    }
+    auto sum(size_t length) const {
+        size_t index = length;
+        auto retval = T();
+        while (index != 0) {
+            retval += this->m_bit[index - 1];
+            index -= lsb(index);
+        }
+        return retval;
+    }
+    auto sum(size_t begin, size_t end) const {
+        // sum between begin ... end-1
+        return this->sum(end) - this->sum(begin);
+    }
+    auto at(size_t index) const {
+        return this->sum(index, index + 1);
+    }
+
+    size_t size() const {
+        return this->m_bit.size();
+    }
+
+    auto begin() const {
+        return this->m_bit.cbegin();
+    }
+    auto cbegin() const {
+        return this->m_bit.cbegin();
+    }
+    auto end() const {
+        return this->m_bit.cend();
+    }
+    auto cend() const {
+        return this->m_bit.cend();
+    }
+private:
+    static auto lsb(size_t i) {
+        return i & -i;
+    }
+    vector<T> m_bit;
+};
+
+namespace internal {
+    template <typename T>
+    struct oneline<BinaryIndexedTree<T>> {
+        std::string operator()(const BinaryIndexedTree<T> &t) const {
+            Vector<T> xs;
+            REP (i, t.size()) {
+                xs.push_back(t.at(i));
+            }
+            return oneline<decltype(xs)>()(xs);
+        }
+    };
+}
+
+
 void body() {
     auto N = read<i64>();
     auto M = read<i64>();
@@ -819,7 +909,7 @@ void body() {
     /* xs[i][j] = 都市iと都市i+1の間を通り、都市0から都市jに含まれる列車の本数 */
     auto xs = make_matrix<i64, 2>({N + 1, N + 1}, 0);
     /* ys[i] = 都市0から都市iに含まれる列車の本数*/
-    auto ys = Vector<i64>(N + 1);
+    auto ys = BinaryIndexedTree<i64>(N + 1);
 
     REP (i, M) {
         auto L = LRs[i].first;
@@ -830,9 +920,7 @@ void body() {
                 xs[j][k] += 1;
             }
         }
-        FOR (j, R, N + 1) {
-            ys[j] += 1;
-        }
+        ys.add(R, 1);
     }
 
     EACH (pq, pqs) {
@@ -844,6 +932,6 @@ void body() {
          * - 0からp-1に含まれる列車の本数
          * - [p-1, p]を通る列車の本数
          */
-        cout << ys[q] - ys[p - 1] - xs[p - 1][q] << endl;
+        cout << ys.at(q) - ys.at(p - 1) - xs[p - 1][q] << endl;
     }
 }
