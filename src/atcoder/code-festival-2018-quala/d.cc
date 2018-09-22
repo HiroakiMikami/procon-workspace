7d92deb4cc38b30193ecd9151de39754f012a1be
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-09-22 13:33:52 +0000

    [procon test] Run test src/atcoder/code-festival-2018-quala/d.cc

--- src/atcoder/code-festival-2018-quala/d.cc
+++ src/atcoder/code-festival-2018-quala/d.cc
@@ -1085,6 +1085,7 @@ void body() {
             auto f = elem.first;
             auto n = elem.second;
             auto dist = Xs[i] - ((i == 0) ? 0 : Xs[i - 1]);
+            dump(i, f, dist, n);
             if (f - dist < 0) continue;
 
             if ((f - dist) >= T) {
