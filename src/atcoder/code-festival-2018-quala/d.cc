b7a8b80af1f46c70200f69e43c326e3d6bb070a4
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-09-23 06:48:54 +0000

    [procon test] Run test src/atcoder/code-festival-2018-quala/d.cc

--- src/atcoder/code-festival-2018-quala/d.cc
+++ src/atcoder/code-festival-2018-quala/d.cc
@@ -1089,6 +1089,7 @@ void body() {
          * k, k+1, ..., i: X_{i+1}で補給する条件から一意（すべて建て替える必要あり）
          */
         REP (j, i) {
+            if (F - (Xs[i] - Xs[j]) < 0) continue;
             if (F - (Xs[i] - Xs[j]) >= T) continue;
             size_t k_ = i;
             FOR (k, j + 1, i) {
