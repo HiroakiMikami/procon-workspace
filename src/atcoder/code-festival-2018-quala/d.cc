8d5064b07e8ce6996ee895c649b0ea02077d8662
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-09-23 07:31:53 +0000

    [procon test] Run test src/atcoder/code-festival-2018-quala/d.cc

--- src/atcoder/code-festival-2018-quala/d.cc
+++ src/atcoder/code-festival-2018-quala/d.cc
@@ -1167,7 +1167,7 @@ void body() {
          * k, k+1, ..., i: X_{i+1}で補給する条件から一意（すべて建て替える必要あり）
          */
         auto minimum = std::max<i64>(0, j_min[i]);
-        auto maximum = j_max[i] + 1;
+        auto maximum = j_max[i];
         if (maximum > minimum) {
             dump(i, maximum, minimum);
             dp[i] += sum_dp[maximum + 1] - sum_dp[minimum];
