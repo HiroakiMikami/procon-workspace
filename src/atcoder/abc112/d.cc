5a0037ac6bb175667a1d13e8889c239bdef3d36f
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-10-06 12:17:32 +0000

    [procon test] Run test src/atcoder/abc112/d.cc

--- src/atcoder/abc112/d.cc
+++ src/atcoder/abc112/d.cc
@@ -798,6 +798,138 @@ namespace std {
 #define MAIN
 void body();
 
+#ifndef MAIN
+#include "common.cc"
+#endif
+
+/*
+ * n^r
+ */
+template<class V>
+static V pow(V n, i64 r) {
+    if (r == 0) {
+        return 1;
+    }
+
+    auto r2 = r / 2;
+    auto x2 = pow(n, r2);
+
+    return x2 * x2 * ((r % 2 == 0) ? 1 : n);
+}
+
+static i64 gcd(i64 a, i64 b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+template<class Iterator>
+static i64 gcd_ctr(const Iterator &begin, const Iterator &end) {
+    if (begin == end) {
+        return -1;
+    } else {
+        auto ans = *begin;
+        auto it = begin;
+        ++it;
+        for (; it != end; ++it) {
+            auto x = *it;
+            ans = gcd(ans, x);
+        }
+        return ans;
+    }
+}
+
+static i64 gcd_ctr(const Vector<i64> &xs) {
+    return gcd_ctr(xs.begin(), xs.end());
+}
+
+static i64 lcm(i64 a, i64 b) {
+    auto x = gcd(a, b);
+    return a / x * b;
+}
+
+template<class Iterator>
+static i64 lcm_ctr(const Iterator &begin, const Iterator &end) {
+    if (begin == end) {
+        return -1;
+    } else {
+        auto ans = *begin;
+        auto it = begin;
+        ++it;
+        for (; it != end; ++it) {
+            auto x = *it;
+            ans = lcm(ans, x);
+        }
+        return ans;
+    }
+}
+
+static i64 lcm_ctr(const Vector<i64> &xs) {
+    return lcm_ctr(xs.begin(), xs.end());
+}
+
+template<class V>
+static V combination(V n, i64 r) {
+    if (r == 0) {
+        return 1;
+    }
+
+    V x = 1;
+    FOR (d, 1, r + 1) {
+        x *= n;
+        n -= 1;
+        x /= d;
+    }
+    return x;
+}
+
+static bool is_prime(i64 n) {
+    if (n <= 1) return false;
+    for (i64 i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+static Vector<i64> divisor(i64 n) {
+    Vector<i64> res;
+    for (i64 i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i != n / i) res.push_back(n / i);
+        }
+    }
+    return res;
+}
+
+static HashMap<i64, size_t> prime_factor(i64 n) {
+    HashMap<i64, size_t> res;
+    for (i64 i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            res[i] += 1;
+            n /= i;
+        }
+    }
+    if (n != 1) res[n] = 1;
+    return res;
+}
+
+static pair<Vector<i64>, Vector<bool>> sieve(i64 n) {
+    Vector<i64> prime;
+    Vector<bool> is_prime_(n + 1, true);
+    is_prime_[0] = is_prime_[1] = false;
+
+    FOR (i, 2, n + 1) {
+        if (is_prime_[i]) {
+            prime.push_back(i);
+            for (i64 j = 2 * i; j <= n; j += i) {
+                is_prime_[j] = false;
+            }
+        }
+    }
+    return {prime, is_prime_};
+}
+
+
 // main function (DO NOT EDIT)
 int main (int argc, char **argv) {
     cin.tie(0);
@@ -811,11 +943,26 @@ int main (int argc, char **argv) {
 
 void body() {
     auto N = read<i64>();
-    if (N == 1) {
-        cout << "Hello World" << endl;
-    } else {
-        auto A = read<i64>();
-        auto B = read<i64>();
-        cout << (A + B) << endl;
+    auto M = read<i64>();
+
+    /*
+     * sum a_i = Mかつgcd(a_i)=kとすると、
+     * a_i = k * b_iとして、k * sum b_i = Mかつgcd(b_i)=1
+     * a_i > 0より、b_i > 0だから、sum b_i >= N
+     * sum b_i >= Nならば、gcd(b_i)=1となるb_iのとり方がある（b_1を除いて1とすれば良い）
+     */
+
+    auto divisor = divisors(M);
+    divisor.push_back(M);
+
+    i64 ans = 1;
+    EACH (k, divisor) {
+        auto sum_b = M / k;
+        if (sum_b >= N) {
+            ans = std::max(ans, k);
+        }
     }
+
+    cout << ans << endl;
+
 }
