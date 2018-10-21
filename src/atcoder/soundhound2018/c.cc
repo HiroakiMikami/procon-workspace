a22d44cd8d62597a7ff6e07667855882f39b2702
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-10-21 08:17:54 +0000

    [procon test] Run test src/atcoder/soundhound2018/c.cc

--- src/atcoder/soundhound2018/c.cc
+++ src/atcoder/soundhound2018/c.cc
@@ -1310,9 +1310,5 @@ void body() {
      * → 二部グラフなので最大マッチングを使って求められる
      */
     auto f = graph::ford_fulkerson(g, start, goal);
-    if (n % 2 == 0) {
-        cout << f << endl;
-    } else {
-        cout << (f + 1) << endl;
-    }
+    cout << (n - f) << endl;
 }
