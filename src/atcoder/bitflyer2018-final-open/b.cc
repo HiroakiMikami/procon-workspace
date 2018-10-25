7755e49d2760f7120bb10c39144e3596811fd14b
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-10-25 12:04:35 +0000

    AC BitFlyer2018-Final B

--- src/atcoder/bitflyer2018-final-open/b.cc
+++ src/atcoder/bitflyer2018-final-open/b.cc
@@ -1,11 +1,11 @@
 /*
-URL https://
-SCORE 0
-AC false
+URL https://beta.atcoder.jp/contests/bitflyer2018-final-open/tasks/bitflyer2018_final_b
+SCORE 400
+AC true
 WA false
 TLE false
 MLE false
-TASK_TYPE
+TASK_TYPE 累積和 クエリ処理 場合分け
 FAILURE_TYPE
 NOTES
 */
