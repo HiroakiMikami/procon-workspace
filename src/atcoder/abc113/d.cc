995138a96e1c1d429e548869230389ad34f75bad
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-11-10 01:57:24 +0000

    [procon test] Run test src/atcoder/abc113/d.cc

--- src/atcoder/abc113/d.cc
+++ src/atcoder/abc113/d.cc
@@ -1125,7 +1125,7 @@ void body() {
                  *    w
                  */
                 if (w == 0 && !b[w]) {
-                    dump(b.count());
+                    dump(h, w, b.count());
                     dp[h][w] += dp[h - 1][w];
                 } else if (w == W - 1 && !b[w - 1]) {
                     dp[h][w] += dp[h - 1][w];
