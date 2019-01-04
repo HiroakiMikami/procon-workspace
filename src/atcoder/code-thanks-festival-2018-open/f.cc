7c64657fcb4ba919553ba6e842d31d0d410dea75
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-01-04 12:17:11 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2018-open/f.cc

--- src/atcoder/code-thanks-festival-2018-open/f.cc
+++ src/atcoder/code-thanks-festival-2018-open/f.cc
@@ -933,7 +933,7 @@ void body() {
     // 最後は丁度K回にしないといけない
     auto C = cands(v);
     REP (i, N) {
-        dump(C[i], cost[i], K);
+        dump(int(C[i]), cost[i], K);
         if (C[i] && cost[i] == K) {
             v.push_back(i);
             break;
