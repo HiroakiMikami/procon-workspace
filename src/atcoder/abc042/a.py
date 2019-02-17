b4585f17385769cb37f1e960fd9d6eb7f18d2ebf
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-02-17 06:34:40 +0000

    [procon test] Run test src/atcoder/abc042/a.py

---
+++ src/atcoder/abc042/a.py
@@ -0,0 +1,13 @@
+A, B, C = [int(i) for i in input().split()]
+
+five = 0
+seven = 0
+for x in [A, B, C]:
+    if x == 5:
+        five += 1
+    elif x == 7:
+        seven += 1
+
+ans = five == 2 and seven == 1
+
+print("YES" if ans else "NO")
