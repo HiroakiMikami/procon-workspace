b1f491ccd3aa05debbbef8ebdd5dfa706453c471
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-02-20 11:42:22 +0000

    [procon test] Run test src/atcoder/abc054/a.py

---
+++ src/atcoder/abc054/a.py
@@ -0,0 +1,13 @@
+A, B = [ x for x in input().split() ]
+
+if A == B:
+    ans = "Draw"
+elif A == 1:
+    ans = "Alice"
+elif B == 1:
+    ans = "Bob"
+else:
+    ans = "Alice" if A > B else "Bob"
+
+print(ans)
+print()
