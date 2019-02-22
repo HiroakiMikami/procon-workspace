374694d16802e993f2da261df7633068fe106502
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-02-22 11:42:23 +0000

    [procon test] Run test src/atcoder/abc062/a.py

--- src/atcoder/abc062/a.py
+++ src/atcoder/abc062/a.py
@@ -2,8 +2,8 @@ x, y = [ int(x) for x in input().split() ]
 
 def group(x):
     g = {
-        0: set([1, 3, 5, 7, 8, 10, 12])
-        1: set([4, 6, 9, 11])
+        0: set([1, 3, 5, 7, 8, 10, 12]),
+        1: set([4, 6, 9, 11]),
         2: set([2])
     }
     for k in g.keys():
