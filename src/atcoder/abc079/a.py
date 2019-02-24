d7eec4299827e2b6061909e9dd71a5cfd754028f
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-02-24 04:01:15 +0000

    [procon test] Run test src/atcoder/abc079/a.py

--- src/atcoder/abc079/a.py
+++ src/atcoder/abc079/a.py
@@ -1,7 +1,6 @@
 N = int(input())
 
-xs = [ N / 1000, (N / 100) % 10, (N / 10) % 10, N % 10]
-print(xs)
+xs = [ N // 1000, (N // 100) % 10, (N // 10) % 10, N % 10]
 ans = xs[0] == xs[1] == xs[2] or xs[1] == xs[2] == xs[3]
 
 print("Yes" if ans else "No")
