ec44d31824b870a81f2f55508d3b25bc7584ccef
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-02 02:58:16 +0000

    [procon test] Run test src/atcoder/abc097/a.py

--- src/atcoder/abc097/a.py
+++ src/atcoder/abc097/a.py
@@ -1,6 +1,6 @@
 a, b, c, d = [ int(x) for x in input().split() ]
 
-ans = (abs(b - a) <= d and abs(b - c)) or abs(c - a) <= d
+ans = (abs(b - a) <= d and abs(b - c) <= d) or abs(c - a) <= d
 
 print("Yes" if ans else "No")
 print()
