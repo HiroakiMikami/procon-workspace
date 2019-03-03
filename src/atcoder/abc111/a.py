e3eecb243d6fdae21715a741b55a0a3890fac3f7
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-03 04:55:51 +0000

    [procon test] Run test src/atcoder/abc111/a.py

--- src/atcoder/abc111/a.py
+++ src/atcoder/abc111/a.py
@@ -3,11 +3,11 @@ n = int(input())
 ans = 0
 for i in range(3):
     if str(n)[i] == "1":
-        ans = 9 * pow(10, 2 - i)
+        ans += 9 * pow(10, 2 - i)
     elif str(n)[i] == "9":
-        ans = 1 * pow(10, 2 - i)
+        ans += 1 * pow(10, 2 - i)
     else:
-        ans = int(str(n)[i]) * pow(10, 2 - i)
+        ans += int(str(n)[i]) * pow(10, 2 - i)
 
 print(ans)
 print()
