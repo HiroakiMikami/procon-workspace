a9551d64552d42ef26ac333838a0c79023bf24c2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-03 12:02:34 +0000

    [procon test] Run test src/atcoder/abc120/b.py

---
+++ src/atcoder/abc120/b.py
@@ -0,0 +1,12 @@
+A, B, K = [ int(x) for x in input().split() ]
+
+cnt = 0
+for i in range(1, min(A, B)):
+    if A % i == 0 and B % i == 0:
+        cnt += 1
+        if cnt == K:
+            ans = i
+            break
+
+print(ans)
+print()
