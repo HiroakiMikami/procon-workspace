0c1cb36af977d1ce8bccaf7c77325ee03c188c85
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-04 12:08:15 +0000

    [procon test] Run test src/atcoder/cf16-final-open/a.py

---
+++ src/atcoder/cf16-final-open/a.py
@@ -0,0 +1,10 @@
+H, W = [ int(x) for input().split() ]
+Ss = [ input().split() for _ in range(H) ]
+
+for i in range(H):
+    for j in range(W):
+        if Ss[i][j] == "snuke":
+            ans = "{}{}".format(chr(j + 'A'), i + 1)
+
+print(ans)
+print()
