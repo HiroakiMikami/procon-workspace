3551c8928983913e812aef07b7229dfab0ed7256
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-05 10:35:58 +0000

    [procon test] Run test src/atcoder/code-festival-2016-qualc/a.py

---
+++ src/atcoder/code-festival-2016-qualc/a.py
@@ -0,0 +1,11 @@
+s = input()
+
+x = ""
+for ch in s:
+    if x == "" and ch == "C":
+        x = "C"
+    elif x = "C" and ch == "F":
+        x = "CF"
+ans = x == "CF"
+
+print("Yes" if ans else "No")
