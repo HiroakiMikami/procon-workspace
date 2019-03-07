8a20c26d30226f49a033246bd964c4db6f41d5a3
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-07 10:45:12 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2018-open/a.py

---
+++ src/atcoder/code-thanks-festival-2018-open/a.py
@@ -0,0 +1,13 @@
+T, A, B, C, D = [ int(x) for x in input().split() ]
+
+if T >= A + C:
+    ans = B + D
+elif T >= C:
+    ans = D
+elif T >= A:
+    ans = B
+else:
+    ans = 0
+
+print(ans)
+print()
