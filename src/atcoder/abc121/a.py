e9e06f526a094288961e26a2834e2184f0409785
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-09 12:02:15 +0000

    [procon test] Run test src/atcoder/abc121/a.py

--- src/atcoder/abc121/a.py
+++ src/atcoder/abc121/a.py
@@ -1,5 +1,5 @@
-H, W = [ int(x) for x in input.split() ]
-h, w = [ int(x) for x in input.split() ]
+H, W = [ int(x) for x in input().split() ]
+h, w = [ int(x) for x in input().split() ]
 
 ans = H * W - (H + W - h * w)
 
