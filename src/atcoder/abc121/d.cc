28c1c00d0cad418800996d73bf28b43c4fb35a70
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-10 07:12:59 +0000

    [procon test] Run test src/atcoder/abc121/d.cc

--- src/atcoder/abc121/d.cc
+++ src/atcoder/abc121/d.cc
@@ -827,6 +827,7 @@ void body() {
             x = (X / 4) * 4 - 1; // f(0, 4X+3) = 0が成り立つ
         }
         i64 ans = 0;
+        dump(x, X);
         FOR (i, x + 1, X + 1) {
             ans = ans ^ i;
         }
