cf3913d25ac2b60f6bea808968b12c0791672da2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-11 06:44:28 +0000

    [procon test] Run test src/atcoder/abc047/b.py

---
+++ src/atcoder/abc047/b.py
@@ -0,0 +1,30 @@
+W, H, N = [ int(x) for x in input().split() ]
+xyas = [ input().split() for _ in range(N) ]
+
+area = [ [ 0 for _ in range(W) ] for _ in range(H) ]
+for xya in xyas:
+    x, y, a = xya
+    if a == 1:
+        for i in range(x):
+            for j in rnage(H):
+                area[j][i] = 1
+    elif a == 2:
+        for i in range(x, W):
+            for j in rnage(H):
+                area[j][i] = 1
+    elif a == 3:
+        for i in range(W):
+            for j in rnage(y):
+                area[j][i] = 1
+    else:
+        for i in range(W):
+            for j in rnage(y, H):
+                area[j][i] = 1
+ans = 0
+for s in area:
+    for t in s:
+        if t == 0:
+            ans += 1
+
+print(ans)
+
