496ceaefe0838e5671a4358fac2818330d385ee2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-15 12:23:03 +0000

    [procon test] Run test src/atcoder/abc051/b.py

---
+++ src/atcoder/abc051/b.py
@@ -0,0 +1,10 @@
+K, S = [ int(x) for x in input().split() ]
+
+ans = 0
+for X in range(K + 1):
+    for Y in range(K + 1):
+        Z = S - X - Y
+        if 0 <= Z and Z <= K:
+            ans += 1
+
+print(ans)
