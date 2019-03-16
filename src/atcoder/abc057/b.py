f21acae2ebc032182a29b0525dc9d71060b3b13d
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-16 04:46:18 +0000

    [procon test] Run test src/atcoder/abc057/b.py

---
+++ src/atcoder/abc057/b.py
@@ -0,0 +1,18 @@
+N, M = [ int(x) for x in input().split() ]
+ab_s = [ [ int(x) for x in input().split() ] for _ in range(N) ]
+cds = [ [ int(x) for x in input().split() ] for _ in range(M) ]
+
+ans = []
+for i in range(N):
+    a, b = ab_s[i]
+    r = -1
+    d_min = -1
+    for j in range(M):
+        c, d = cds[j]
+        distance = abs(a - c) + abs(b - d)
+        if d_min < 0 or d_min > distance:
+            r = j
+            d_min = distance
+    ans.append(r)
+
+print("\n".join(map(str, ans)))
