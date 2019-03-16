abde0d8e4ece45e69c1e9ba64c388d0e260eb35f
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-16 04:57:24 +0000

    [procon test] Run test src/atcoder/abc062/b.py

---
+++ src/atcoder/abc062/b.py
@@ -0,0 +1,13 @@
+H, W = [ int(x) for x in input().split() ]
+a_s = [ input() for _ in range(H) ]
+
+ans = ""
+for i in range(W + 2):
+    ans += "#"
+ans += "\n"
+for a in a_s:
+    ans += "#" + a + "#"
+for i in range(W + 2):
+    ans += "#"
+
+print(ans)
