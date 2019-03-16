d406c94ca240466272e425ffbc7bf5cac58849c2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-16 04:37:46 +0000

    [procon test] Run test src/atcoder/abc054/b.py

---
+++ src/atcoder/abc054/b.py
@@ -0,0 +1,17 @@
+N, M = [ int(x) for x in input().split() ]
+As = [ input() for _ in range(N) ]
+Bs = [ input() for _ in range(M) ]
+
+def contain(i, j):
+    for p in range(M):
+        if As[i + p][j:j+M] != Bs[p]:
+            return False
+    return True
+
+ans = False
+for i in range(N):
+    for j in range(N):
+        if contain(i, j):
+            ans = True
+
+print("Yes" if ans else "No")
