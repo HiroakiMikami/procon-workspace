bb082b04f206c1e7266b307d8582f8b9d8fc89a0
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-17 03:53:00 +0000

    [procon test] Run test src/atcoder/abc065/b.py

--- src/atcoder/abc065/b.py
+++ src/atcoder/abc065/b.py
@@ -3,9 +3,10 @@ a_s = [ int(input()) for _ in range(N) ]
 
 b = 1
 ans = None
-for i in range(N + 1):
+for i in range(N):
     b = a_s[b - 1]
     if b == 2:
         ans = i + 1
+        break
 
 print(-1 if ans is None else ans)
