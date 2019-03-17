dd7aeb405dda042cbdb0cbee80d4c65bc7441c0b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-17 04:05:03 +0000

    [procon test] Run test src/atcoder/abc070/b.py

--- src/atcoder/abc070/b.py
+++ src/atcoder/abc070/b.py
@@ -1,10 +1,14 @@
 A, B, C, D = [ int(x) for x in input().split() ]
 
-if A <= C and B >= C:
-    ans = B - C
-elif C <= A and D >= A:
-    ans = D - A
+if A <= C:
+    if B <= D:
+        ans = max(0, B - C)
+    else:
+        ans = max(0, D - C)
 else:
-    ans = 0
+    if B <= D:
+        ans = max(0, B - A)
+    else:
+        ans = max(0, D - A)
 
 print(ans)
