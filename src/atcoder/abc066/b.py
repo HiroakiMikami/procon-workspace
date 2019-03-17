501680a9b401a7b6054bb663cba658df82c54fcb
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-17 03:55:11 +0000

    [procon test] Run test src/atcoder/abc066/b.py

--- src/atcoder/abc066/b.py
+++ src/atcoder/abc066/b.py
@@ -3,7 +3,7 @@ S = input()
 for i in range(len(S) - 1):
     s = S[0:len(S) - i]
     print(s)
-    if len(s) % 2 == 0 and s[0:len(S)//2] == s[len(S)//2]:
+    if len(s) % 2 == 0 and s[0:len(S)//2] == s[len(S)//2:]:
         ans = s
         break
 
