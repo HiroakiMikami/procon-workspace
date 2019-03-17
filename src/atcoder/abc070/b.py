1e2a105c18db185ccbf40c3d069c40e76c318239
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-17 04:02:01 +0000

    [procon test] Run test src/atcoder/abc070/b.py

--- src/atcoder/abc070/b.py
+++ src/atcoder/abc070/b.py
@@ -1,8 +1,8 @@
 A, B, C, D = [ int(x) for x in input().split() ]
 
-if A + B >= C:
+if A <= C and A + B >= C:
     ans = (A + B) - C
-elif C + D >= A:
+elif C <= A and C + D >= A:
     ans = (C + D) - A
 else:
     ans = 0
