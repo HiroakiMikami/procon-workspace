00812f87c24a9856d1ee79c92495a0ec34e67420
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-23 05:19:38 +0000

    [procon test] Run test src/atcoder/abc078/b.py

--- src/atcoder/abc078/b.py
+++ src/atcoder/abc078/b.py
@@ -3,7 +3,6 @@ X, Y, Z = [ int(x) for x in input().split() ]
 ans = 0
 for i in range(1, X + 1):
     if (i + 1) * Z + i * Y <= X:
-        print((i + 1) * Z + i * Y, X)
         ans = i
 
-print(i)
+print(ans)
