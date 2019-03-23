be976cdde79f0df3fa639bb81da8fdaac4470482
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-23 05:27:57 +0000

    [procon test] Run test src/atcoder/abc081/b.py

--- src/atcoder/abc081/b.py
+++ src/atcoder/abc081/b.py
@@ -4,11 +4,11 @@ As = [ int(x) for x in input().split() ]
 ans = 0
 while True:
     finished = False
-    for A in As:
+    for i, A in enumerate(As):
         if A % 2 == 1:
             finished = True
             break
-        A /= 2
+        As[i] /= 2
     if finished:
         break
     ans += 1
