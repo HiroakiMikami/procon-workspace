d0cd7503004bed0b75030b394a35213dd10086a0
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-23 05:12:18 +0000

    [procon test] Run test src/atcoder/abc075/b.py

---
+++ src/atcoder/abc075/b.py
@@ -0,0 +1,18 @@
+H, W = [ int(x) for x in input().split() ]
+Ss = [ input() for _ in range(H) ]
+
+ans = Ss.copy()
+for y in range(H):
+    for x in range(W):
+        if ans[y][x] == "#":
+            continue
+        n = 0
+        for dir in [[0, 1], [0, -1], [1, 0], [1, 1], [1, -1], [-1, 0], [-1, 1], [-1, -1]]:
+            x2 = x + dir[0]
+            y2 = y + dir[1]
+            if x2 < 0 or x2 >= W or y2 < 0 or y2 >= H:
+                continue
+            n += 1 if Ss[y2][x2] == "#" else 0
+        ans[y][x] = str(n)
+
+print("\n".join(ans))
