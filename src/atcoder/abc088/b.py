d24e15e86cb1b21303b9746098e9e67904f20d10
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-24 00:51:37 +0000

    [procon test] Run test src/atcoder/abc088/b.py

--- src/atcoder/abc088/b.py
+++ src/atcoder/abc088/b.py
@@ -2,9 +2,10 @@ N = int(input())
 a_s = [int(x) for x in input().split()]
 
 a_s.sort()
+a_s.reverse()
 alice = 0
 bob = 0
-for i, a in enumerate(a_s.reverse()):
+for i, a in enumerate(a_s):
     if i % 2 == 0:
         alice += a
     else:
