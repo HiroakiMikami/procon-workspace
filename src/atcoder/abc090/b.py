97a0b66401ee8538b4d0acc052e54618578cbad9
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-24 00:54:47 +0000

    [procon test] Run test src/atcoder/abc090/b.py

---
+++ src/atcoder/abc090/b.py
@@ -0,0 +1,13 @@
+A, B = [int(x) for x in input().split()]
+
+ans = 0
+for i in range(A, B + 1):
+    xs = []
+    x = i
+    while x > 0:
+        xs.append(x % 10)
+        x //= 10
+    if xs == list(xs.reversed()):
+        ans += 1
+
+print(ans)
