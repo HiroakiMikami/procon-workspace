82ad0c91aa0d676b9c8141e280d60b98b3c3d667
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-26 10:43:16 +0000

    [procon test] Run test src/atcoder/abc094/b.py

--- src/atcoder/abc094/b.py
+++ src/atcoder/abc094/b.py
@@ -1,8 +1,8 @@
 N, M, X = [int(x) for x in input().split()]
 As = [int(x) for x in input().split()]
 
-c1 = len(filter(lambda A: A < X), As)
-c2 = len(filter(lambda A: A > X), As)
+c1 = len(filter(lambda A: A < X, As))
+c2 = len(filter(lambda A: A > X, As))
 ans = min(c1, c2)
 
 print(ans)
