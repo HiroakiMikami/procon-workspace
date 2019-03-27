f015b7da1eb9e9fda32ed1dc3adf287a60fdf06a
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-27 12:16:03 +0000

    [procon test] Run test src/atcoder/abc100/b.py

---
+++ src/atcoder/abc100/b.py
@@ -0,0 +1,13 @@
+D, N = [int(x) for x in input().split()]
+
+ans = 1
+for _ in range(D):
+    ans *= 100
+k = 0
+for i in range(N):
+    k += 1
+    if k % 10 == 0:
+        k += 1
+ans *= k
+
+print(ans)
