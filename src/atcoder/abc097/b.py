152bb5436cd79c4b43538af75192921d76dda064
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-27 12:10:15 +0000

    [procon test] Run test src/atcoder/abc097/b.py

--- src/atcoder/abc097/b.py
+++ src/atcoder/abc097/b.py
@@ -4,12 +4,10 @@ from math import sqrt
 for i in range(1, X + 1):
     if i == 1:
         ans = i
-    print(int(sqrt(i)) + 1, i)
     for b in range(2, int(sqrt(i)) + 1):
         tmp = b * b
         while tmp < i:
             tmp *= b
-        print(tmp, b)
         if tmp == i:
             ans = i
 
