865bb9cfa4a280cad6d6001fc8fbfc446b0413dd
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-30 05:38:31 +0000

    [procon test] Run test src/atcoder/abc109/b.py

--- src/atcoder/abc109/b.py
+++ src/atcoder/abc109/b.py
@@ -2,14 +2,12 @@ N = int(input())
 Ws = [input() for _ in range(N)]
 
 ans = True
-w = set(Ws[0])
+w = set([Ws[0]])
 for i in range(1, N):
     if Ws[i - 1][-1] != Ws[i][0]:
-        print(Ws[i], "hoge")
         ans = False
         break
     if Ws[i] in w:
-        print(Ws[i], w)
         ans = False
         break
     w.add(Ws[i])
