ee195fc748eee770eb425111cbb37b7aa31ae86d
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-30 05:33:06 +0000

    [procon test] Run test src/atcoder/abc108/b.py

---
+++ src/atcoder/abc108/b.py
@@ -0,0 +1,9 @@
+x1, y1, x2, y2 = [int(x) for x in input().split()]
+
+v = [x2 - x1, y2 - y1]
+v_rot = [-v[1],  v[0]]
+x3, y3 = [x2 + v_rot[0], y2 + v_rot[1]]
+x4, y4 = [x1 + v_rot[0], y1 + v_rot[1]]
+ans = [x3, y3, x4, y4]
+
+print(" ".join(ans))
