f54303655db2963ed63f09bda94cfae04a3255fa
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-30 05:37:47 +0000

    [procon test] Run test src/atcoder/abc109/b.py

--- src/atcoder/abc109/b.py
+++ src/atcoder/abc109/b.py
@@ -5,9 +5,13 @@ ans = True
 w = set(Ws[0])
 for i in range(1, N):
     if Ws[i - 1][-1] != Ws[i][0]:
+        print(Ws[i])
         ans = False
+        break
     if Ws[i] in w:
+        print(Ws[i])
         ans = False
+        break
     w.add(Ws[i])
 
 print("Yes" if ans else "No")
