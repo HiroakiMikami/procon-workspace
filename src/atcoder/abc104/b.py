8d9a3917ef5098da06bf8c6dca917998ae38ae05
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-30 05:13:38 +0000

    [procon test] Run test src/atcoder/abc104/b.py

--- src/atcoder/abc104/b.py
+++ src/atcoder/abc104/b.py
@@ -5,7 +5,7 @@ if S[0] != "A":
     ans = False
 if S[2:-2].count("C") != 1:
     ans = False
-if len([x for x in S if s.islower()]) != len(S) - 2:
+if len([x for x in S if x.islower()]) != len(S) - 2:
     ans = False
 
 print("AC" if ans else "WA")
