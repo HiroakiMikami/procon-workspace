8996faaab408edfe6e5eac337a82996ba9340a53
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-30 05:23:14 +0000

    [procon test] Run test src/atcoder/abc106/b.py

---
+++ src/atcoder/abc106/b.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+ans = 0
+for i in range(1, N, 2):
+    n = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            n += 1
+    if n == 8:
+        ans += 1
+
+print(ans)
