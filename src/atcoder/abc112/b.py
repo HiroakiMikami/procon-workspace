cc899ac59eee78f654b3a8b70ca88389d68ace4b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-31 00:40:27 +0000

    [procon test] Run test src/atcoder/abc112/b.py

--- src/atcoder/abc112/b.py
+++ src/atcoder/abc112/b.py
@@ -1,6 +1,10 @@
 N, T = [ int(x) for x in input().split() ]
 cts = [[ int(x) for x in input().split() ] for _ in range(N)]
 
-ans = min(map(lambda ct: ct[0], filter(lambda ct: ct[1] <= T, cts)))
+cands = list(map(lambda ct: ct[0], filter(lambda ct: ct[1] <= T, cts)))
+if len(cands) == 0:
+    ans = "TLE"
+else:
+    ans = min(cands)
 
 print(ans)
