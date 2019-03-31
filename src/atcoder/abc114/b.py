d0c952e6a026e7f207c11a2e5f9e942e208113c8
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-31 00:43:58 +0000

    [procon test] Run test src/atcoder/abc114/b.py

---
+++ src/atcoder/abc114/b.py
@@ -0,0 +1,12 @@
+S = input()
+
+ans = -1
+for i in range(len(S) - 3):
+    X = int(S[i:i + 3])
+    d = abs(X - 753)
+    if ans < 0:
+        ans = d
+    else:
+        ans = min(ans, d)
+
+print(ans)
