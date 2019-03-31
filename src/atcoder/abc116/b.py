ebd9417010aa00d22acbceeb167133d218756ac0
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-03-31 00:49:31 +0000

    [procon test] Run test src/atcoder/abc116/b.py

---
+++ src/atcoder/abc116/b.py
@@ -0,0 +1,19 @@
+s = int(input())
+
+def f(n):
+    if n % 2 == 0:
+        return n / 2
+    else:
+        return 3 * n + 1
+a_s = [s]
+A = set([s])
+for m in range(1, 1000000 + 1):
+    a_m = f(a_s[-1])
+    if a_m in A:
+        ans = m + 1
+        break
+
+    a_s.append(a_m)
+    A.add(a_m)
+
+print(ans)
