433a0e892cce236e13cc3a68878cabce9cccf7a2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-03 12:45:00 +0000

    [procon test] Run test src/atcoder/dwacon5th-prelims/b.cc

--- src/atcoder/dwacon5th-prelims/b.cc
+++ src/atcoder/dwacon5th-prelims/b.cc
@@ -824,7 +824,7 @@ void body() {
     }
 
     i64 ans = 0;
-    REPR(i, 64) {
+    REPR(i, 40) {
         // 上の桁から順に決める
         i64 n = 0;
         auto t = ans + (1 << i);
@@ -836,7 +836,7 @@ void body() {
 
         if (n >= K) {
             // うまく選ぶとibit目をたてることができる
-            ans |= (1 << i);
+            ans += (1 << i);
         }
     }
     cout << ans << endl;
