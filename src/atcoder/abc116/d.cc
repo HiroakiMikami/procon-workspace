49c2056b6e305eb0b7a10a03f6a5d19c29340933
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-04 10:42:12 +0000

    [procon test] Run test src/atcoder/abc116/d.cc

--- src/atcoder/abc116/d.cc
+++ src/atcoder/abc116/d.cc
@@ -857,7 +857,6 @@ void body() {
 
     i64 ans = point;
     auto next = [&](auto _next) {
-        dump(point);
         pair<i64, i64> to_delete = {-1, -1};
         while (!q1.empty()) {
             auto q = q1.top();
