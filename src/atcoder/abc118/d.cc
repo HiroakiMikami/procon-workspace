e0f6f9b73833487eb81c1857673c15ca6e847003
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-05 12:30:12 +0000

    [procon test] Run test src/atcoder/abc118/d.cc

--- src/atcoder/abc118/d.cc
+++ src/atcoder/abc118/d.cc
@@ -836,16 +836,18 @@ void body() {
 
     // Create the number (which digit is dp(N))
     std::string ans = "";
+    i64 n = N;
     REP (i, dp[N]) {
         // Decide (dp[N]-i)-th digit
         i64 x = 0;
         EACH (A, As) {
-            if (i - num[A] < 0) continue;
-            if (dp[i - num[A]] == dp[N] - i - 1) {
+            if (n - num[A] < 0) continue;
+            if (dp[n - num[A]] == dp[N] - i - 1) {
                 x = std::max(x, A);
             }
         }
         ans += str[x];
+        n -= num[x];
     }
 
     std::cout << ans << std::endl;
