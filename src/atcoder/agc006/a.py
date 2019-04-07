2e2b0538309491da5153334d6b5f9cc9dd94b0d2
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-07 00:28:16 +0000

    [procon test] Run test src/atcoder/agc006/a.py

--- src/atcoder/agc006/a.py
+++ src/atcoder/agc006/a.py
@@ -3,8 +3,7 @@ s = input()
 t = input()
 
 common = 0
-for i in range(min(len(s), len(t))):
-    print(s[-i:], t[:i])
+for i in range(min(len(s) + 1, len(t) + 1)):
     if s[-i:] == t[:i]:
         common = i
 ans = len(s + t[common:])
