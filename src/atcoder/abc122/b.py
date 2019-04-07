e809a9f4266fc1d002c633e97ac6985867c255dc
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-07 00:05:21 +0000

    [procon test] Run test src/atcoder/abc122/b.py

--- src/atcoder/abc122/b.py
+++ src/atcoder/abc122/b.py
@@ -4,7 +4,7 @@ ans = 0
 for i in range(len(S)):
     for j in range(i, len(S) + 1):
         s = set(S[i:j])
-        if s <= set(["A", "C", "G", T]):
+        if s <= set(["A", "C", "G", "T"]):
             ans = max(len(S[i:j]), ans)
 
 print(ans)
