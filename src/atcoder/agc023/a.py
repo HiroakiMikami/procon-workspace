869a1924a734fbaf766bb08bdb29c73a248c07b9
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-20 07:25:52 +0000

    [procon test] Run test src/atcoder/agc023/a.py

--- src/atcoder/agc023/a.py
+++ src/atcoder/agc023/a.py
@@ -2,7 +2,9 @@ N = int(input())
 As = [int(x) for x in input().split()]
 
 Ss = [As[0]]
-nums = {As[0]: 1}
+nums = {}
+nums[As[0]] += 1
+nums[0] += 1
 for A in As[1:]:
     S = Ss[-1] + A
     Ss.append(S)
