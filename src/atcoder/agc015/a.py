59c6a35c6527e6d989ebe61071a307242933a8cc
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-20 07:38:03 +0000

    [procon test] Run test src/atcoder/agc015/a.py

--- src/atcoder/agc015/a.py
+++ src/atcoder/agc015/a.py
@@ -3,7 +3,7 @@ N, A, B = [int(x) for x in input().split()]
 if N == 1:
     ans = 1 if A == B else 0
 else:
-    if B > A:
+    if B >= A:
         ans = ((N - 1) * B + A) - ((N - 1) * A + B) + 1
     else:
         ans = 0
