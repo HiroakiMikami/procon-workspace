d49f1778a15b81ceeeced70c64aaed63ec7125de
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-20 07:32:45 +0000

    [procon test] Run test src/atcoder/agc017/a.py

---
+++ src/atcoder/agc017/a.py
@@ -0,0 +1,18 @@
+N, P = [int(x) for x in input().split()]
+As = [int(x) for x in input().split()]
+
+def combination(n, r):
+    ans = 1
+    for i in range(n, n - r, -1):
+        ans *= i
+    for i in range(r):
+        ans /= (i + 1)
+    return ans
+n_e = list(filter(lambda x: x % 2 == 0, As)).count()
+n_o = N - n_e
+
+ans = 0
+for i in range((1 if P == 1 else 0), n_o + 1, 2):
+    ans += pow(2, n_e) * combination(n_o, i)
+
+print(ans)
