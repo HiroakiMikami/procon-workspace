5edfbd5ecfddfa737d5be9ed2307a71c43024ee0
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-20 07:09:45 +0000

    [procon test] Run test src/atcoder/agc027/a.py

--- src/atcoder/agc027/a.py
+++ src/atcoder/agc027/a.py
@@ -4,11 +4,13 @@ a_s = [int(x) for x in input().split()]
 a_s.sort()
 ans = 0
 X = 0
-for a in a_s:
+for a in a_s[:-2]:
     if X + a <= x:
         X += a
         ans += 1
     else:
         break
+if a_s[-1] == X - x:
+    ans += 1
 
 print(ans)
