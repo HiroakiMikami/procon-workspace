c5d3acd73c055f74fdafc11414960a9926399f52
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-21 08:20:56 +0000

    [procon test] Run test src/atcoder/code-festival-2016-qualc/b.py

--- src/atcoder/code-festival-2016-qualc/b.py
+++ src/atcoder/code-festival-2016-qualc/b.py
@@ -1,13 +1,7 @@
 K, T = [int(x) for x in input().split()]
 a_s = [int(x) for x in input().split()]
 
-nums = {}
-for a in a_s:
-    if not a in nums:
-        nums[a] = 0
-    nums[a] += 1
-print(nums)
-a_max = max(nums.values())
+a_max = max(a_s)
 a_others = K - a_max
 ans = max(0, a_max - a_others)
 
