31ba8ba19198717623dcf4a38ca0604fcce4110a
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-21 08:35:49 +0000

    [procon test] Run test src/atcoder/aising2019/b.py

--- src/atcoder/aising2019/b.py
+++ src/atcoder/aising2019/b.py
@@ -2,11 +2,9 @@ N = int(input())
 A, B = [int(x) for x in input().split()]
 Ps = [int(x) for x in input().split()]
 
-Ps.sort()
-ans = 0
-for i, P in enumerate(Ps):
-    n1 = len(list(filter(lambda x: x < P, Ps[:i])))
-    n2 = len(list(filter(lambda x: x > P, Ps[i+1:])))
-    ans += n1 * n2
+n1 = len([x for x in Ps if x <= A])
+n2 = len([x for x in Ps if A < x <= B])
+n3 = len([x for x in Ps if B + 1 <= x])
+ans = n1 * n2 * n3
 
 print(ans)
