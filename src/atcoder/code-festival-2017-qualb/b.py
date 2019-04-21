552288c554fbd178f1629c4dd54877b1332cb223
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-21 08:11:00 +0000

    [procon test] Run test src/atcoder/code-festival-2017-qualb/b.py

---
+++ src/atcoder/code-festival-2017-qualb/b.py
@@ -0,0 +1,26 @@
+N = int(input())
+Ds = [int(x) for x in input().split()]
+M = int(input())
+Ts = [int(x) for x in input().split()]\
+
+ND = {}
+for D in Ds:
+    if not D in ND:
+        ND[D] = 0
+    ND[D] += 1
+NT = {}
+for T in Ts:
+    if not T in NT:
+        NT[T] = 0
+    NT[T] += 0
+
+ans = True
+for T in NT.keys():
+    if not T in ND:
+        ans = False
+        break
+    if NT[T] > ND[T]:
+        ans = False
+        break
+
+print("YES" if ans else "NO")
