da47e24743f9d579b42374cf0a65bf42640b0820
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-21 08:03:09 +0000

    [procon test] Run test src/atcoder/abc123/b.py

--- src/atcoder/abc123/b.py
+++ src/atcoder/abc123/b.py
@@ -5,7 +5,7 @@ D = int(input())
 E = int(input())
 
 Xs = [A, B, C, D, E]
-Xs.sort(lambda x, y: (x % 10) < (y % 10))
+Xs = sorted(Xs, key = lambda x: x % 10)
 ans = 0
 for X in Xs[:-1]:
     ans += (X + 9) // 10 * 10
