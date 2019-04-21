2b1a35d46ef921e9bdb67b59ba3c63c1ffa71ccc
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-21 08:36:22 +0000

    [procon test] Run test src/atcoder/aising2019/b.py

--- src/atcoder/aising2019/b.py
+++ src/atcoder/aising2019/b.py
@@ -5,6 +5,6 @@ Ps = [int(x) for x in input().split()]
 n1 = len([x for x in Ps if x <= A])
 n2 = len([x for x in Ps if A < x <= B])
 n3 = len([x for x in Ps if B + 1 <= x])
-ans = n1 * n2 * n3
+ans = min(n1, n2, n3)
 
 print(ans)
