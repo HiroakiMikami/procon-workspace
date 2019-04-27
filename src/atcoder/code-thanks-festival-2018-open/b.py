f40512ac347d76652b3a7ce6fc78558afa811755
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:33:49 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2018-open/b.py

--- src/atcoder/code-thanks-festival-2018-open/b.py
+++ src/atcoder/code-thanks-festival-2018-open/b.py
@@ -1,6 +1,14 @@
 X, Y = [int(x) for x in input().split()]
 
 x = 3 * X - Y
-ans = x >= 0 and x % 8 == 0
+if x < 0 or x % 8 != 0:
+    ans = False
+else:
+    x //= 8
+    a = X - 3 * x
+    if a < 0:
+        ans = False
+    else:
+        ans = True
 
 print("Yes" if ans else "No")
