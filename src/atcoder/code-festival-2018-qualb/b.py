30e45135620ad343008ddfb19301a02d554d7538
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:10:57 +0000

    [procon test] Run test src/atcoder/code-festival-2018-qualb/b.py

--- src/atcoder/code-festival-2018-qualb/b.py
+++ src/atcoder/code-festival-2018-qualb/b.py
@@ -1,5 +1,7 @@
 N, X = [int(x) for x in input().split()]
 abs = [[int(x) for x in input().split()] for _ in range(N)]
 
-abs = sorted(abs, key=lambda x: x[1])
-print(abs)
+abs = sorted(abs, key=lambda x: -x[1])
+ans = sum(map(lambda x: x[0] * x[1], abs)) + abs[0][1] * X
+
+print(ans)
