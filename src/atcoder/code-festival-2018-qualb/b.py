4acfbf8d3f4753f38e4f4e056dc7c84ecc14c57b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:09:53 +0000

    [procon test] Run test src/atcoder/code-festival-2018-qualb/b.py

--- src/atcoder/code-festival-2018-qualb/b.py
+++ src/atcoder/code-festival-2018-qualb/b.py
@@ -1,10 +1,5 @@
-N, M, A, B = [int(x) for x in input().split()]
-LRs = [[int(x) for x in input().split()] for _ in range(M)]
+N, X = [int(x) for x in input().split()]
+abs = [[int(x) for x in input().split()] for _ in range(N)]
 
-n = [B] * N
-for L, R in LRs:
-    for i in range(L - 1, R):
-        n[i] = A
-ans = sum(n)
-
-print(ans)
+sort(abs, key=lambda x: x[1])
+print(abs)
