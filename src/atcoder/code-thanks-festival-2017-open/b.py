8cca88da2340c456cc6c6736e82bf16c64350ad8
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:24:10 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2017-open/b.py

--- src/atcoder/code-thanks-festival-2017-open/b.py
+++ src/atcoder/code-thanks-festival-2017-open/b.py
@@ -1,13 +1,14 @@
 S = input()
 
 ans = len(S) - 1
-for i in range(len(S)):
+for i in range(1, len(S)):
     former = S[:i]
     former = "".join(list(reversed(former)))
     S_ = S[i:]
-    print(former, S_)
     if former.startswith(S_):
-        ans = 2 * i - len(S)
-        break
+        ans = min(ans, 2 * i - len(S))
+    S__ = S[i - 1:]
+    if former.startswith(S__):
+        ans = min(ans, 2 * i - 1 - len(S))
 
 print(ans)
