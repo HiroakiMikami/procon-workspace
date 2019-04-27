fedfcff8828eef3e9ce959bf7eb17874ba73e44b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:19:37 +0000

    [procon test] Run test src/atcoder/code-thanks-festival-2017-open/b.py

--- src/atcoder/code-thanks-festival-2017-open/b.py
+++ src/atcoder/code-thanks-festival-2017-open/b.py
@@ -2,7 +2,7 @@ S = input()
 
 for i in range(len(S) + 1):
     former = S[:i]
-    former = str(reversed(former))
+    former = "".join(list(reversed(former)))
     S_ = S[i:]
     print(former, S_)
     if former.startswith(S_):
