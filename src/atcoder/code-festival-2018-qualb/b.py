0e1838f4dbd30ba3cc680de647db64a53b6bad5b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-04-27 07:10:16 +0000

    [procon test] Run test src/atcoder/code-festival-2018-qualb/b.py

--- src/atcoder/code-festival-2018-qualb/b.py
+++ src/atcoder/code-festival-2018-qualb/b.py
@@ -1,5 +1,5 @@
 N, X = [int(x) for x in input().split()]
 abs = [[int(x) for x in input().split()] for _ in range(N)]
 
-sorted(abs, key=lambda x: x[1])
+abs = sorted(abs, key=lambda x: x[1])
 print(abs)
