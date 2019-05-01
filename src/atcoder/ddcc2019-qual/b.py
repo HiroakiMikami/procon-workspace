329a78b8472110c1e144d536256baab7dac56975
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:33:57 +0000

    [procon test] Run test src/atcoder/ddcc2019-qual/b.py

--- src/atcoder/ddcc2019-qual/b.py
+++ src/atcoder/ddcc2019-qual/b.py
@@ -6,8 +6,8 @@ if N % 2 == 0:
         ans += 4 * i
 else:
     ans = 0
-    for i in range(N // 2 - 1):
-        ans += 4 * i
+    for i in range(N // 2):
+        ans += 2 + (i - 1) * 2
     ans += N - 2
 
 print(ans)
