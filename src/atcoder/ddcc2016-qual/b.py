0ed01515fb74d95ce980383a2d27aab7ab790c08
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:22:11 +0000

    [procon test] Run test src/atcoder/ddcc2016-qual/b.py

--- src/atcoder/ddcc2016-qual/b.py
+++ src/atcoder/ddcc2016-qual/b.py
@@ -1,7 +1,7 @@
 R, N, M = [int(x) for x in input().split()]
 
 def length(i):
-    if i > N:
+    if i < 0 or i > N:
         return 0
     x = i * 2.0 * R / N
     r = abs(R  - x)
@@ -10,7 +10,7 @@ def length(i):
 
 ans = 0
 for i in range(1, N + M):
-    print(max(length(i), length(i + M)))
-    ans += max(length(i), length(i + M))
+    print(max(length(i), length(i - M)))
+    ans += max(length(i), length(i - M))
 
 print(ans)
