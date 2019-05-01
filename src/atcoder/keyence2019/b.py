50f800b0d49af1551f57a2d1393751daf4b91793
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:42:14 +0000

    [procon test] Run test src/atcoder/keyence2019/b.py

--- src/atcoder/keyence2019/b.py
+++ src/atcoder/keyence2019/b.py
@@ -1,11 +1,11 @@
 S = input()
 
-import re
 s = "keyence"
 ans = s in S
 for i in range(1, len(s)):
     s1 = s[:i]
     s2 = s[i:]
-    ans = ans or re.match(".*{}.*{}".format(s1, s2), S)
+    if S.startswith(s1) and S.endswith(s2):
+        ans = True
 
 print("YES" if ans else "NO")
