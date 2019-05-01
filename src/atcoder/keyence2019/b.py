66051f32d4480a9b766ed57762deee8d931e1083
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:38:43 +0000

    [procon test] Run test src/atcoder/keyence2019/b.py

--- src/atcoder/keyence2019/b.py
+++ src/atcoder/keyence2019/b.py
@@ -1,7 +1,11 @@
 S = input()
 
+import re
 s = "keyence"
-for i in range(len(s)):
+ans = s in S
+for i in range(1, len(s)):
     s1 = s[:i]
     s2 = s[i:]
-    print(s1, s2)
+    ans = ans or re.match(".*{}.*{}".format(s1, s2), S)
+
+print("YES" if ans else "NO")
