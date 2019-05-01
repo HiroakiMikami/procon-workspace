820ebac4c07e400b8f19bae46365ead89a64594b
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:30:14 +0000

    [procon test] Run test src/atcoder/ddcc2019-qual/b.py

---
+++ src/atcoder/ddcc2019-qual/b.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+if N % 2 == 0:
+    ans = 0
+    for i in range(N // 2):
+        ans += 4 * i
+else:
+    ans = 0
+    for i in range(N // 2):
+        ans += 4 * i
+    ans += N - 2
+
+print(ans)
