46fb58a69f444a8d9d1174907735337bec0e5fae
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-01 06:19:13 +0000

    [procon test] Run test src/atcoder/ddcc2016-qual/b.py

---
+++ src/atcoder/ddcc2016-qual/b.py
@@ -0,0 +1,13 @@
+R, N, M = [int(x) for x in input().split()]
+
+def length(i):
+    x = i * 2.0 * R / N
+    r = abs(R  - x)
+    from math import sqrt
+    return sqrt(R * R - r * r)
+
+ans = 0
+for i in range(1, N + M):
+    ans += max(length(i), length(i + M))
+
+print(ans)
