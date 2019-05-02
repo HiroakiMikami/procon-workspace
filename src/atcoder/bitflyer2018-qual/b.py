b0dcafb9cf845a447f41055f51e3c8735beae3d4
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-02 04:44:29 +0000

    [procon test] Run test src/atcoder/bitflyer2018-qual/b.py

---
+++ src/atcoder/bitflyer2018-qual/b.py
@@ -0,0 +1,16 @@
+A, B, N = [int(x) for x in input().split()]
+X = input()
+
+for x in X:
+    if x == "S":
+        A = max(A - 1, 0)
+    elif x == "C":
+        B = max(B - 1, 0)
+    else:
+        if A >= B:
+            A = max(A - 1, 0)
+        else:
+            B = max(B - 1, 0)
+ans = (A, B)
+
+print(" ".join(map(str, ans)))
