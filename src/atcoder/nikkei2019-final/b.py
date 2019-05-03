63ac094271f42274e093cfe38fe4b86497cc41b0
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-03 00:30:14 +0000

    [procon test] Run test src/atcoder/nikkei2019-final/b.py

--- src/atcoder/nikkei2019-final/b.py
+++ src/atcoder/nikkei2019-final/b.py
@@ -6,7 +6,10 @@ X = 0
 for i, A in enumerate(As):
     X *= K
     X += A
-Y = sum(map(lambda x: x[1] * pow(K, M - x[0] - 1), enumerate(Bs)))
+Y = 0
+for i, B in enumerate(Bs):
+    Y *= K
+    Y += B
 ans = "X" if X < Y else "Y" if X > Y else "Same"
 
 print(ans)
