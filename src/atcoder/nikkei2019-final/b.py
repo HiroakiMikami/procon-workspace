3a0b6c76da88b498217ae7ac884ec2ea1968aa2f
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-03 00:29:54 +0000

    [procon test] Run test src/atcoder/nikkei2019-final/b.py

--- src/atcoder/nikkei2019-final/b.py
+++ src/atcoder/nikkei2019-final/b.py
@@ -2,18 +2,10 @@ N, M, K = [int(x) for x in input().split()]
 As = [int(x) for x in input().split()]
 Bs = [int(x) for x in input().split()]
 
-def pow(i, j):
-    if j == 0:
-        return 1
-    if j == 1:
-        return i
-    if j % 2 == 0:
-        t = pow(i, j // 2)
-        return t * t
-    else:
-        return i * pow(i, j - 1)
-
-X = sum(map(lambda x: x[1] * pow(K, N - x[0] - 1), enumerate(As)))
+X = 0
+for i, A in enumerate(As):
+    X *= K
+    X += A
 Y = sum(map(lambda x: x[1] * pow(K, M - x[0] - 1), enumerate(Bs)))
 ans = "X" if X < Y else "Y" if X > Y else "Same"
 
