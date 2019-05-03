d4515c1af29787c8d6b9704805e3daffa569e689
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-03 00:25:51 +0000

    [procon test] Run test src/atcoder/nikkei2019-final/b.py

--- src/atcoder/nikkei2019-final/b.py
+++ src/atcoder/nikkei2019-final/b.py
@@ -4,7 +4,7 @@ Bs = [int(x) for x in input().split()]
 
 from math import pow
 X = sum(map(lambda x: x[1] * pow(K, N - x[0] - 1), enumerate(As)))
-Y = sum(map(lambda x: x[1] * pow(K, N - x[0] - 1), enumerate(Bs)))
+Y = sum(map(lambda x: x[1] * pow(K, M - x[0] - 1), enumerate(Bs)))
 ans = "X" if X < Y else "Y" if X > Y else "Same"
 
 print(ans)
