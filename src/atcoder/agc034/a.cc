e67dcb499d4615e4b7135d2a6c4092976afd6c73
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-09-20 21:52:49 +0900

    [procon test] Run test src/atcoder/agc034/a.cc

--- src/atcoder/agc034/a.cc
+++ src/atcoder/agc034/a.cc
@@ -821,7 +821,6 @@ void body() {
         auto ans = Vector<std::experimental::optional<bool>>(N - s); // ans[i] = i + sへsからいけるかどうか
         ans[0] = true;
         auto f = [&](i64 x, auto _f) -> bool {
-            dump(x);
             // s -> xまでいけるかどうか
             if (ans[x - s]) {
                 return ans[x - s].value();
