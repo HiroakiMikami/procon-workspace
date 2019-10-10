c8369569605536a2361a17527208ee907171ac44
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-10-10 22:06:30 +0900

    [procon test] Run test src/atcoder/abc127/e.cc

--- src/atcoder/abc127/e.cc
+++ src/atcoder/abc127/e.cc
@@ -1139,11 +1139,11 @@ void body() {
 
     FOR (d, 1, N) {
         auto tmp = ModInteger<>(d * (M - d) * N * N);
-        ans += tmp * fact[N * M - 2] / fact[K - 2] / fact[N * M - 2 - (K - 2)];
+        ans += tmp * fact[N * M - 2].value * fact[K - 2].inverse * fact[N * M - 2 - (K - 2)].inverse;
     }
     FOR (d, 1, M) {
         auto tmp = ModInteger<>(d * (N - d) * M * M);
-        ans += tmp * fact[N * M - 2] / fact[K - 2] / fact[N * M - 2 - (K - 2)];
+        ans += tmp * fact[N * M - 2].value * fact[K - 2].inverse * fact[N * M - 2 - (K - 2)].inverse;
     }
 
     cout << ans.get() << endl;
