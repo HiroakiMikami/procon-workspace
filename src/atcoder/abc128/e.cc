2658d3fd58a2bddc56f5cf98154836d634d3d378
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-10-31 22:26:18 +0900

    [procon test] Run test src/atcoder/abc128/e.cc

--- src/atcoder/abc128/e.cc
+++ src/atcoder/abc128/e.cc
@@ -835,6 +835,7 @@ void body() {
     OrderedSet<i64> Xs;
     EACH (E, Es) {
         dump(Ds[i], get<0>(E));
+        dump(Xs);
         if (Ds[i] < get<0>(E)) {
             if (Xs.empty()) {
                 cout << -1 << endl;
