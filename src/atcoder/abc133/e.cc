e03dc0aec2ce387a5e8ac14185e29fd4328b2515
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-11-15 21:37:10 +0900

    [procon test] Run test src/atcoder/abc133/e.cc

--- src/atcoder/abc133/e.cc
+++ src/atcoder/abc133/e.cc
@@ -1133,11 +1133,11 @@ void visit(auto K, auto &g, auto &ans, auto &visited, i64 v, i64 n_1, i64 n_2) {
     if (visited[v]) {
         return ;
     }
+    visited[v] = true;
     dump(v, n_1, n_2);
     ans *= (K - n_1 - n_2);
     auto i = 0;
     EACH (u, g[v]) {
-        visited[u] = true;
         visit(K, g, ans, visited, u, 1, n_1 + i);
         i += 1;
     }
