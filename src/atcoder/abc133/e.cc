157ff0dda2fa04d47c3f55b096b0a787a7b454d5
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-11-15 21:35:43 +0900

    [procon test] Run test src/atcoder/abc133/e.cc

--- src/atcoder/abc133/e.cc
+++ src/atcoder/abc133/e.cc
@@ -1130,6 +1130,7 @@ namespace internal {
 }
 
 void visit(auto K, auto &g, auto &ans, auto &visited, i64 v, i64 n_1, i64 n_2) {
+    dump(v, n_1, n_2);
     ans *= (K - n_1 - n_2);
     auto i = 0;
     EACH (u, g[v]) {
