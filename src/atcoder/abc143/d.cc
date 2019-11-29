384e7842b4430317c5b54b2f95ecc4cb37e8282e
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-11-29 22:01:40 +0900

    [procon test] Run test src/atcoder/abc143/d.cc

--- src/atcoder/abc143/d.cc
+++ src/atcoder/abc143/d.cc
@@ -822,9 +822,6 @@ void body() {
 
             // max(a, b) <= c < a + bを満たすcの数を調べる
             auto E = std::lower_bound(Ls.begin() + j + 1, Ls.end(), a + b);
-            if (E == Ls.end()) {
-                continue ;
-            }
             dump(a, b);
             dump(Ls);
             dump(E - Ls.begin());
