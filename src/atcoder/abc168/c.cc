1e97c53d254f79939cabb68c05e078d8eda5fc57
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-05-29 19:35:32 +0900

    [procon test] Run test src/atcoder/abc168/c.cc

--- src/atcoder/abc168/c.cc
+++ src/atcoder/abc168/c.cc
@@ -817,7 +817,7 @@ void body() {
     auto H = read<i64>();
     auto M = read<i64>();
 
-    auto d = (60 * H - 71 * M) / 72.0 / 30.0 * M_PI;
+    auto d = (60 * H - 11 * M) / 12.0 / 30.0 * M_PI;
     dump(d / M_PI);
     cout << std::sqrt(A * A + B * B - 2 * A * B * std::cos(d)) << endl;
 }
