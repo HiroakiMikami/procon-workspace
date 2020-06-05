c25a2e705a4387146110fb94e4a431967eafc19e
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-06-05 20:06:11 +0900

    [procon test] Run test src/atcoder/abc166/d.cc

--- src/atcoder/abc166/d.cc
+++ src/atcoder/abc166/d.cc
@@ -1025,6 +1025,7 @@ void body() {
 
     // A^5 - (-B)^5 = Xの場合
     dump(find_root_5(X));
+    throw "???";
     REP (A, find_root_5(X) + 2) {
         auto x = X - pow(A, 5);
         if (x < 0) continue ;
