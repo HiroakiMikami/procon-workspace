8f44b830dc2518ef1f7db959d6316d9324561e8f
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-03 20:12:48 +0900

    [procon test] Run test src/atcoder/abc171/c.cc

--- src/atcoder/abc171/c.cc
+++ src/atcoder/abc171/c.cc
@@ -814,7 +814,7 @@ void body() {
     auto ans = Vector<char>();
     while (N != 0) {
         ans.push_back((N % 23) + 'a' - 1);
-        N /= 23;
+        N /= 26;
     }
     std::reverse(CTR(ans));
 
