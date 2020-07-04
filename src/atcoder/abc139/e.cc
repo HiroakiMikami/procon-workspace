6ae57b4bf9719b5690f3ea945310bce5fb4cce88
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-04 21:35:50 +0900

    [procon test] Run test src/atcoder/abc139/e.cc

--- src/atcoder/abc139/e.cc
+++ src/atcoder/abc139/e.cc
@@ -1341,48 +1341,52 @@ namespace graph {
 
 namespace graph {
     template <typename EdgeLabel, typename Container>
-    std::optional<std::vector<Edge<EdgeLabel>>> cycle(const Graph<EdgeLabel, Container> &g, size_t start) {
+    std::optional<std::vector<Edge<EdgeLabel>>> cycle(const Graph<EdgeLabel, Container> &g) {
         using _Edge = Edge<EdgeLabel>;
         std::vector<_Edge> stack;
         std::optional<std::vector<_Edge>> retval;
         auto visited = Vector<bool>(g.vertices_size(), false);
 
-        dfs(g, {start},
-                 [&retval, &stack, &visited](const auto &edge_opt, size_t v) {
-                     dump(stack);
-                     if (!edge_opt) return false;
-                     if (visited[v]) return false;
-                     auto edge = edge_opt.value();
-                     while (!stack.empty()) {
-                         auto l = stack.back();
-                         if (get<1>(l) == get<0>(edge)) {
-                             break;
-                         }
-                         stack.pop_back();
-                     }
+        REP (i, g.vertices_size()) {
+            if (visited[i]) {
+                continue ;
+            }
+            dfs(g, {i},
+                    [&retval, &stack, &visited](const auto &edge_opt, size_t v) {
+                        if (!edge_opt) return false;
+                        if (visited[v]) return false;
+                        auto edge = edge_opt.value();
+                        while (!stack.empty()) {
+                            auto l = stack.back();
+                            if (get<1>(l) == get<0>(edge)) {
+                                break;
+                            }
+                            stack.pop_back();
+                        }
 
-                     // find backward path
-                     for (auto &e: stack) {
-                         if (get<0>(e) == get<1>(edge)) {
-                             retval = std::make_optional(std::vector<_Edge>());
-                         }
+                        // find backward path
+                        for (auto &e: stack) {
+                            if (get<0>(e) == get<1>(edge)) {
+                                retval = std::make_optional(std::vector<_Edge>());
+                            }
 
-                         if (retval) {
-                             retval.value().push_back(e);
-                         }
-                     }
+                            if (retval) {
+                                retval.value().push_back(e);
+                            }
+                        }
 
-                     visited[v] = true;
+                        visited[v] = true;
 
-                     if (retval) {
-                         retval.value().push_back(edge);
-                         return true;
-                     }
+                        if (retval) {
+                            retval.value().push_back(edge);
+                            return true;
+                        }
 
-                     stack.push_back(edge);
-                     return false;
-                 },
-                 filter_nothing(g));
+                        stack.push_back(edge);
+                        return false;
+                    },
+                    filter_nothing(g));
+        }
 
         return retval;
     }
